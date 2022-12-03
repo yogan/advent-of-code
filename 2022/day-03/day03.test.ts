2956da4233f60dfe0512735888be2b3c6bed702c
Frank Blendinger <yogan@zogan.de>
2022-12-03 23:40:42 +0100

    Add 2022 day 03 part 2 (TS/Bun)
    This requires an upgrade to bun canary (0.3.0), so that we can
    conveniently compare arrays and objects in test with .toEqual(), which
    is not yet available in bun 0.2.x.

--- 2022/day-03/day03.test.ts
+++ 2022/day-03/day03.test.ts
@@ -1,5 +1,14 @@
-import { expect, test } from "bun:test"
-import { toPriority, part1 } from "./day03"
+import { test, expect } from "bun:test"
+import { toPriority, splitIntoGroups, readLines, part1, part2 } from "./day03"
+
+// we are using bun canary (0.3.0) which brings .toEqual (and other nice stuff),
+// but bun-types are not available yet (still at 0.2.2), so we'll have to extend
+// them a bit here.
+declare module "bun:test" {
+    interface Expect {
+        toEqual(value: any): void
+    }
+}
 
 test("toPriority", () => {
     expect(toPriority("a")).toBe(1)
@@ -8,6 +17,32 @@ test("toPriority", () => {
     expect(toPriority("Z")).toBe(52)
 })
 
+test("splitIntoGroups", () => {
+    const groups = splitIntoGroups([
+        "a1",
+        "a2",
+        "a3",
+        "b1",
+        "b2",
+        "b3",
+        "c1",
+        "c2",
+        "c3",
+    ])
+
+    expect(groups).toEqual([
+        ["a1", "a2", "a3"],
+        ["b1", "b2", "b3"],
+        ["c1", "c2", "c3"],
+    ])
+})
+
+const lines = readLines("./day03.sample")
+
 test("day 3 part 1 works", () => {
-    expect(part1("day03.sample")).toBe(157)
+    expect(part1(lines)).toBe(157)
+})
+
+test("day 3 part 2 works", () => {
+    expect(part2(lines)).toBe(70)
 })
--- 2022/day-03/day03.ts
+++ 2022/day-03/day03.ts
@@ -1,12 +1,15 @@
 import { readFileSync } from "node:fs"
 
-const readLines = (filename: PathOrFileDescriptor) =>
+export const readLines = (filename: PathOrFileDescriptor) =>
     readFileSync(filename, { encoding: "utf-8" })
         .split("\n")
         .filter((line) => line.trim().length > 0)
 
 const intersection = <T>(s1: Set<T>, s2: Set<T>) =>
     new Set(Array.from(s1).filter((x) => s2.has(x)))
+
+const intersection3 = <T>(s1: Set<T>, s2: Set<T>, s3: Set<T>) =>
+    intersection(intersection(s1, s2), s3)
 
 const extractFromSet = <T>(set: Set<T>): T => Array.from(set)[0]
 
@@ -20,7 +23,7 @@ export const toPriority = (letter: string) => {
         : ascii - offsetUppercase
 }
 
-const splitRucksack = (rucksack: string) => {
+const splitIntoCompartments = (rucksack: string) => {
     const middle = rucksack.length / 2
     return [
         new Set(rucksack.slice(0, middle)),
@@ -28,12 +31,42 @@ const splitRucksack = (rucksack: string) => {
     ] as const
 }
 
-export const part1 = (filename: string) =>
-    readLines(filename)
-        .map(splitRucksack)
+type Group<T> = [T, T, T]
+
+export const splitIntoGroups = <T>(lines: T[]): Group<T>[] => {
+    function splitRec(list: T[], out: Group<T>[]): void {
+        if (list.length === 0) {
+            return
+        }
+
+        const [a, b, c, ...rest] = list
+        out.push([a, b, c])
+
+        splitRec(rest, out)
+    }
+
+    const groups: Group<T>[] = []
+    splitRec(lines, groups)
+    return groups
+}
+
+export const part1 = (lines: string[]) =>
+    lines
+        .map(splitIntoCompartments)
         .map(([left, right]) => intersection(left, right))
         .map(extractFromSet)
         .map(toPriority)
         .reduce((a, b) => a + b, 0)
 
-console.log(`Day 02 part 1: ${part1("./day03.in")}`)
+export const part2 = (lines: string[]) =>
+    splitIntoGroups(lines)
+        .map(([one, two, three]) =>
+            intersection3(new Set(one), new Set(two), new Set(three))
+        )
+        .map(extractFromSet)
+        .map(toPriority)
+        .reduce((a, b) => a + b, 0)
+
+const lines = readLines("./day03.in")
+console.log(`Day 02 part 1: ${part1(lines)}`)
+console.log(`Day 02 part 2: ${part2(lines)}`)
