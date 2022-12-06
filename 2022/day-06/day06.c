f60a8c11616e4dcb1063775f9e7ed42ba8e21c5c
Frank Blendinger <yogan@zogan.de>
2022-12-06 09:31:52 +0100

    Add 2022 day 06 part 2 (C)

--- 2022/day-06/day06.c
+++ 2022/day-06/day06.c
@@ -5,19 +5,18 @@
 
 const char *FILENAME = "day06.in";
 const size_t LINE_SIZE = 4096;
-const size_t WINDOW_SIZE = 4;
 
-bool has_four_unique_chars(const char *str, size_t offset)
+bool has_four_unique_chars(const char *str, size_t offset, size_t window_size)
 {
-    if (str == NULL || strlen(str) < offset + WINDOW_SIZE)
+    if (str == NULL || strlen(str) < offset + window_size)
     {
         return false;
     }
 
-    for (size_t i = offset; i < offset + WINDOW_SIZE; i++)
+    for (size_t i = offset; i < offset + window_size; i++)
     {
         char c = str[i];
-        for (size_t j = offset; j < offset + WINDOW_SIZE; j++)
+        for (size_t j = offset; j < offset + window_size; j++)
         {
             if (i != j && c == str[j])
             {
@@ -29,13 +28,13 @@ bool has_four_unique_chars(const char *str, size_t offset)
     return true;
 }
 
-size_t find_marker(char *line)
+size_t find_marker(char *line, size_t window_size)
 {
     for (size_t i = 0; line[i] != '\0' && line[i] != '\n'; i++)
     {
-        if (has_four_unique_chars(line, i))
+        if (has_four_unique_chars(line, i, window_size))
         {
-            return i + WINDOW_SIZE;
+            return i + window_size;
         }
     }
     printf("No marker found in line: %s", line);
@@ -61,25 +60,34 @@ void read_file(char *line)
     fclose(file);
 }
 
-void test(char *input, size_t expected)
+void test(char *input, size_t window_size, size_t expected)
 {
-    size_t marker = find_marker(input);
+    size_t marker = find_marker(input, window_size);
+
     if (marker != expected)
     {
-        printf("Error (input: \"%s\"): expected marker %zu, got %zu\n",
-               input, expected, marker);
+        printf("Error (input: \"%s\", window: \"%zu\"): expected marker %zu, got %zu\n",
+               input, window_size, expected, marker);
         return;
     }
-    printf("Test passed (input: \"%s\")\n", input);
+
+    printf("Test passed (input: \"%s\", window: \"%zu\")\n",
+           input, window_size);
 }
 
 void run_tests()
 {
-    test("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7);
-    test("bvwbjplbgvbhsrlpgdmjqwftvncz", 5);
-    test("nppdvjthqldpwncqszvftbrmjlhg", 6);
-    test("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10);
-    test("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11);
+    test("mjqjpqmgbljsphdztnvjfqwrcgsmlb", /*   */ 4, 7);
+    test("bvwbjplbgvbhsrlpgdmjqwftvncz", /*     */ 4, 5);
+    test("nppdvjthqldpwncqszvftbrmjlhg", /*     */ 4, 6);
+    test("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", /**/ 4, 10);
+    test("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", /* */ 4, 11);
+
+    test("mjqjpqmgbljsphdztnvjfqwrcgsmlb", /*   */ 14, 19);
+    test("bvwbjplbgvbhsrlpgdmjqwftvncz", /*     */ 14, 23);
+    test("nppdvjthqldpwncqszvftbrmjlhg", /*     */ 14, 23);
+    test("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", /**/ 14, 29);
+    test("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", /* */ 14, 26);
 }
 
 int main()
@@ -89,8 +97,12 @@ int main()
 
     char line[LINE_SIZE];
     read_file(line);
-    size_t marker = find_marker(line);
-    printf("Marker: %ld\n", marker);
+
+    size_t marker1 = find_marker(line, 4);
+    printf("Part 1 (start-of-packet marker):  %ld\n", marker1);
+
+    size_t marker2 = find_marker(line, 14);
+    printf("Part 2 (start-of-message marker): %ld\n", marker2);
 
     return 0;
 }
