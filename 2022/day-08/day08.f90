6a360214a0461cf5c85dce7aa25a68ad85e797d0
Frank Blendinger <yogan@zogan.de>
2022-12-08 11:53:41 +0100

    Parse day 08 input file (Fortran)

--- 2022/day-08/day08.f90
+++ 2022/day-08/day08.f90
@@ -2,9 +2,37 @@ module day08
    implicit none
 contains
 
-   integer function double( x )
-      integer, intent(in) :: x
-      double = x * 2
-   end function double
+   function read_file( filename, grid_size ) result(grid)
+      character(len=*), intent(in) :: filename
+      integer, intent(in) :: grid_size
+      integer :: i, j, n
+      integer :: grid(grid_size, grid_size)
+      character(len=grid_size) :: line
+
+      open( unit=1, file=filename, status='old' )
+      do i = 1, grid_size
+         read( 1, '(a)', iostat=n ) line
+         if ( n /= 0 ) exit
+         do j = 1, grid_size
+            read(line(j:j), *) grid(i,j)
+         end do
+      end do
+      close( 1 )
+
+      grid = grid
+   end function read_file
+
+   subroutine print_grid( grid )
+      integer, intent(in) :: grid(:,:)
+      integer :: row, col
+
+      do row = 1, size(grid, 2)
+         write(*, fmt="(1x,a)", advance="no") "  "
+         do col = 1, size(grid, 1)
+            write(*, fmt="(i0)", advance="no") grid(row, col)
+         end do
+         print *, ''
+       end do
+   end subroutine print_grid
 
 end module day08
--- 2022/day-08/main.f90
+++ 2022/day-08/main.f90
@@ -1,10 +1,18 @@
 program main
    use day08
-
    implicit none
-   integer :: i
 
-   do i = 0, 5
-      print '("double(", i0, ") = ", i02)', i, double(i)
-   end do
+   integer :: sample_grid_size = 5
+   integer :: sample_grid(5, 5)
+   ! integer :: grid_size = 99
+   ! integer :: grid(99, 99)
+
+   sample_grid = read_file("day08.sample", sample_grid_size)
+   print *, "Sample grid:"
+   call print_grid(sample_grid)
+
+   ! grid = read_file("day08.in", grid_size)
+   ! print *, "Grid:"
+   ! call print_grid(grid)
+
 end program main
