f17dedde55792cebd5a3a5e6a740268b97aea8d3
Frank Blendinger <yogan@zogan.de>
2022-12-08 14:20:46 +0100

    Calculate tree visibility (day 08, Fortran)

--- 2022/day-08/day08.f90
+++ 2022/day-08/day08.f90
@@ -22,6 +22,62 @@ contains
       grid = grid
    end function read_file
 
+   function calc_visibility( grid, grid_size ) result(visibility)
+      integer, intent(in) :: grid(:,:)
+      integer, intent(in) :: grid_size
+      logical :: visibility(grid_size, grid_size)
+      integer :: row, col, x
+
+      do row = 1, grid_size
+         do col = 1, grid_size
+            visibility(row, col) = .true.
+         end do
+      end do
+
+      do row = 2, grid_size - 1
+         loop: do col = 2, grid_size - 1
+            visibility(row, col) = .false.
+
+            ! go to the left
+            do x = col - 1, 1, -1
+               if ( grid(row, x) >= grid(row, col) ) then
+                  go to 10
+               end if
+            end do
+            visibility(row, col) = .true.
+            cycle loop
+
+            ! go to the right
+10          do x = col + 1, grid_size, 1
+               if ( grid(row, x) >= grid(row, col) ) then
+                  go to 20
+               end if
+            end do
+            visibility(row, col) = .true.
+            cycle loop
+
+            ! go up
+20          do x = row - 1, 1, -1
+               if ( grid(x, col) >= grid(row, col) ) then
+                  go to 30
+               end if
+            end do
+            visibility(row, col) = .true.
+            cycle loop
+
+            ! go down
+30          do x = row + 1, grid_size, 1
+               if ( grid(x, col) >= grid(row, col) ) then
+                  cycle loop
+               end if
+            end do
+            visibility(row, col) = .true.
+         end do loop
+      end do
+
+      visibility = visibility
+   end function calc_visibility
+
    subroutine print_grid( grid )
       integer, intent(in) :: grid(:,:)
       integer :: row, col
@@ -32,7 +88,20 @@ contains
             write(*, fmt="(i0)", advance="no") grid(row, col)
          end do
          print *, ''
-       end do
+      end do
    end subroutine print_grid
+
+   subroutine print_visibility( visibility )
+      logical, intent(in) :: visibility(:,:)
+      integer :: row, col
+
+      do row = 1, size(visibility, 2)
+         write(*, fmt="(1x,a)", advance="no") "  "
+         do col = 1, size(visibility, 1)
+            write(*, fmt="(L)", advance="no") visibility(row, col)
+         end do
+         print *, ''
+      end do
+   end subroutine print_visibility
 
 end module day08
--- 2022/day-08/tests.f90
+++ 2022/day-08/tests.f90
@@ -2,23 +2,63 @@ program tests
    use day08
    implicit none
 
-   integer :: grid_size = 5
-   integer :: grid(5,5)
+   call test_read_file()
+   call test_calc_visibility()
 
-   integer, dimension(5,5) :: expected_grid = reshape( &
-      (/ 3,0,3,7,3, &
-         2,5,5,1,2, &
-         6,5,3,3,2, &
-         3,3,5,4,9, &
-         3,5,3,9,0 /), &
-      shape(expected_grid), order=(/2,1/) )
+contains
+   subroutine test_read_file()
+      integer :: grid_size = 5
+      integer :: grid(5,5)
 
-   grid = read_file("day08.sample", grid_size)
-   if (any(grid /= expected_grid)) then
-      print *, "Test failed: Expected grid :"
-      call print_grid(expected_grid)
-      print *, "Got grid :"
-      call print_grid(grid)
-   end if
+      integer, dimension(5,5) :: expected_grid = reshape( &
+         (/ 3,0,3,7,3, &
+            2,5,5,1,2, &
+            6,5,3,3,2, &
+            3,3,5,4,9, &
+            3,5,3,9,0 /), &
+         shape(expected_grid), order=(/2,1/) )
+
+      grid = read_file("day08.sample", grid_size)
+
+      if (any(grid /= expected_grid)) then
+         print *, "Test failed: Expected grid:"
+         call print_grid(expected_grid)
+         print *, "Got grid:"
+         call print_grid(grid)
+      else
+         print *, "Test passed (read_file)"
+      end if
+   end subroutine test_read_file
+
+   subroutine test_calc_visibility()
+      integer :: grid_size = 5
+      integer, dimension(5,5):: grid = reshape( &
+         (/ 3,0,3,7,3, &
+            2,5,5,1,2, &
+            6,5,3,3,2, &
+            3,3,5,4,9, &
+            3,5,3,9,0 /), &
+         shape(grid), order=(/2,1/) )
+
+      logical :: visibility(5,5)
+      logical, dimension(5,5) :: expected_visibility = reshape( &
+         (/ .true.,.true.,.true.,.true.,.true., &
+            .true.,.true.,.true.,.false.,.true., &
+            .true.,.true.,.false.,.true.,.true., &
+            .true.,.false.,.true.,.false.,.true., &
+            .true.,.true.,.true.,.true.,.true. /), &
+         shape(expected_visibility), order=(/2,1/) )
+
+      visibility = calc_visibility(grid, grid_size)
+
+      if (any(visibility .neqv. expected_visibility)) then
+         print *, "Test failed: Expected visibility:"
+         call print_visibility(expected_visibility)
+         print *, "Got visibility:"
+         call print_visibility(visibility)
+      else
+         print *, "Test passed (calc_visibility)"
+      end if
+   end subroutine test_calc_visibility
 
 end program tests
