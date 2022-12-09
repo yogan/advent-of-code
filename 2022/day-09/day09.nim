840f28608242fdb7510e77f7bb3894ec66072f12
Frank Blendinger <yogan@zogan.de>
2022-12-09 23:15:03 +0100

    Add steps (day 09 part 1 Nim)

--- 2022/day-09/day09.nim
+++ 2022/day-09/day09.nim
@@ -8,6 +8,13 @@ type
     direction: Direction
     steps: int
 
+type
+  Positions = object
+    head_x: int
+    head_y: int
+    tail_x: int
+    tail_y: int
+
 proc parseInput(filename: string): seq[Command] =
   result = readFile(filename)
     .splitLines()
@@ -23,12 +30,60 @@ proc parseInput(filename: string): seq[Command] =
                                "Invalid direction: " & it[0]),
       steps: parseInt(it[1])))
 
-proc part1: int =
-  # let input = parseInput("day09.sample")
-  let input = parseInput("day09.in")
-  # echo input
+proc step(positions: Positions, direction: Direction): Positions =
+  result = positions
 
-  result = input.len
+  case direction
+  of left: dec(result.head_x)
+  of right: inc(result.head_x)
+  of up: inc(result.head_y)
+  of down: dec(result.head_y)
+
+  var delta_x = result.head_x - result.tail_x
+  var delta_y = result.head_y - result.tail_y
+  assert abs(delta_x) + abs(delta_y) < 4
+
+  if delta_x == 2:
+    inc(result.tail_x)
+  elif delta_x == -2:
+    dec(result.tail_x)
+  elif delta_y == 2:
+    inc(result.tail_y)
+  elif delta_y == -2:
+    dec(result.tail_y)
+
+  var diagonal = (
+    (abs(delta_x) == 2 and abs(delta_y) == 1) or
+    (abs(delta_x) == 1 and abs(delta_y) == 2)
+  )
+  if not diagonal:
+    return
+
+  if (delta_x == 1):
+    inc(result.tail_x)
+  if (delta_x == -1):
+    dec(result.tail_x)
+  if (delta_y == 1):
+    inc(result.tail_y)
+  if (delta_y == -1):
+    dec(result.tail_y)
+
+proc simulateRope(commands: seq[Command]) =
+  var positions = Positions(head_x: 0, head_y: 0,
+                            tail_x: 0, tail_y: 0)
+
+  for command in commands:
+    echo command
+    for _ in 0 ..< command.steps:
+      positions = step(positions, command.direction)
+      echo fmt"H ({positions.head_x}/{positions.head_y})   T ({positions.tail_x}/{positions.tail_y})"
+
+proc part1: int =
+  let commands = parseInput("day09.sample")
+  # let commands = parseInput("day09.in")
+  simulateRope(commands)
+
+  result = 999999999
 
 #region Tests
 
@@ -50,6 +105,58 @@ suite "Advent of Code 2022 Day 09":
       Command(direction: left, steps: 5),
       Command(direction: right, steps: 2)
     ])
+
+  test "step (tail stays)":
+    check(step(
+      Positions(head_x: 0, head_y: 0,
+                tail_x: 0, tail_y: 0), right) ==
+      Positions(head_x: 1, head_y: 0,
+                tail_x: 0, tail_y: 0))
+
+    check(step(
+      Positions(head_x: 2, head_y: 1,
+                tail_x: 1, tail_y: 1), left) ==
+      Positions(head_x: 1, head_y: 1,
+                tail_x: 1, tail_y: 1))
+
+  test "step (tail follows horizontally)":
+    check(step(
+      Positions(head_x: 2, head_y: 1,
+                tail_x: 1, tail_y: 1), right) ==
+      Positions(head_x: 3, head_y: 1,
+                tail_x: 2, tail_y: 1))
+
+    check(step(
+      Positions(head_x: 2, head_y: 1,
+                tail_x: 3, tail_y: 1), left) ==
+      Positions(head_x: 1, head_y: 1,
+                tail_x: 2, tail_y: 1))
+
+  test "step (tail follows vertically)":
+    check(step(
+      Positions(head_x: 1, head_y: 2,
+                tail_x: 1, tail_y: 3), down) ==
+      Positions(head_x: 1, head_y: 1,
+                tail_x: 1, tail_y: 2))
+
+    check(step(
+      Positions(head_x: 1, head_y: 2,
+                tail_x: 1, tail_y: 1), up) ==
+      Positions(head_x: 1, head_y: 3,
+                tail_x: 1, tail_y: 2))
+
+  test "step (tail follows diagonally)":
+    check(step(
+      Positions(head_x: 2, head_y: 2,
+                tail_x: 1, tail_y: 1), up) ==
+      Positions(head_x: 2, head_y: 3,
+                tail_x: 2, tail_y: 2))
+
+    check(step(
+      Positions(head_x: 2, head_y: 2,
+                tail_x: 1, tail_y: 1), right) ==
+      Positions(head_x: 3, head_y: 2,
+                tail_x: 2, tail_y: 2))
 
 #endregion Tests
 
