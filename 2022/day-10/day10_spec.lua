84ae2f13b0340ed5030cf7e9ecd410a340ee7b1d
Frank Blendinger <yogan@zogan.de>
2022-12-11 01:45:29 +0200

    Add 2022 day 10 part 2 (Lua)

--- 2022/day-10/day10_spec.lua
+++ 2022/day-10/day10_spec.lua
@@ -1,16 +1,6 @@
 local day10 = require "day10"
 
 describe("Advent of Code 2022 Day 10", function()
-    it("should parse the short sample input", function()
-        local result = day10.parseInput("day10.sample.short")
-        assert.are.same({
-            cycle = 5,
-            x = -1,
-            signalStrengths = {},
-            sum = 0
-        }, result)
-    end)
-
     it("should calculate the signal strengths for the sample input", function()
         local result = day10.parseInput("day10.sample")
         assert.are.same({420, 1140, 1800, 2940, 2880, 3960}, result.signalStrengths)
@@ -19,5 +9,17 @@ describe("Advent of Code 2022 Day 10", function()
     it("should return the signal strength sum the sample input", function()
         local result = day10.parseInput("day10.sample")
         assert.are.same(13140, result.sum)
+    end)
+
+    it("should draw the final CRT image for the sample input", function()
+        local result = day10.parseInput("day10.sample")
+        assert.are.same(table.concat({
+            "##..##..##..##..##..##..##..##..##..##..",
+            "###...###...###...###...###...###...###.",
+            "####....####....####....####....####....",
+            "#####.....#####.....#####.....#####.....",
+            "######......######......######......####",
+            "#######.......#######.......#######....."
+        }, "\n"), result.crt)
     end)
 end)
