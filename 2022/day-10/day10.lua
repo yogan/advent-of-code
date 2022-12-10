3c5afde8032600b7a21e903f6801d0e76adbbe24
Frank Blendinger <yogan@zogan.de>
2022-12-11 01:45:29 +0200

    Add 2022 day 10 part 2 (Lua)

--- 2022/day-10/day10.lua
+++ 2022/day-10/day10.lua
@@ -1,5 +1,37 @@
 local day10 = {}
 
+function replace_char(pos, str, c)
+    return str:sub(1, pos - 1) .. c .. str:sub(pos + 1)
+end
+
+function string.insert(str1, str2, pos)
+    return str1:sub(1, pos) .. str2 .. str1:sub(pos + 1)
+end
+
+function format(crt)
+    for i = 5, 1, -1 do
+        crt = string.insert(crt, "\n", 40 * i)
+    end
+    return crt
+end
+
+function sum(values)
+    local sum = 0
+    for _, value in ipairs(values) do
+        sum = sum + value
+    end
+    return sum
+end
+
+function update_crt(crt, cycle, sprite_pos)
+    local pos = cycle % (40 * 6) + 1
+    local line_pos = cycle % 40 + 1
+    if (line_pos == sprite_pos or line_pos == sprite_pos + 1 or line_pos == sprite_pos + 2) then
+        return replace_char(pos, crt, "#")
+    end
+    return crt
+end
+
 function day10.parseInput(filename)
     local file = io.open(filename, "r")
     local cycle = 0
@@ -15,17 +47,22 @@ function day10.parseInput(filename)
         [220] = true
     }
 
+    local crt = string.rep(".", 40 * 6)
+
     for line in file:lines() do
         if (line == "noop") then
+            crt = update_crt(crt, cycle, x)
             cycle = cycle + 1
             if (interesting[cycle]) then
                 table.insert(signalStrengths, cycle * x)
             end
         else
+            crt = update_crt(crt, cycle, x)
             cycle = cycle + 1
             if (interesting[cycle]) then
                 table.insert(signalStrengths, cycle * x)
             end
+            crt = update_crt(crt, cycle, x)
             cycle = cycle + 1
             if (interesting[cycle]) then
                 table.insert(signalStrengths, cycle * x)
@@ -37,19 +74,18 @@ function day10.parseInput(filename)
         end
     end
 
-    local sum = 0
-    for _, strength in ipairs(signalStrengths) do
-        sum = sum + strength
-    end
-
     return {
         cycle = cycle,
         x = x,
         signalStrengths = signalStrengths,
-        sum = sum
+        sum = sum(signalStrengths),
+        crt = format(crt)
     }
 end
 
-print("Part 1:", day10.parseInput("day10.in").sum)
+local result = day10.parseInput("day10.in")
+print("Part 1:", result.sum)
+print("Part 2:")
+print(result.crt)
 
 return day10
