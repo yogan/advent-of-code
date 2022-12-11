2f8f752337cea4be3be4781d7c08fd51b759183c
Frank Blendinger <yogan@zogan.de>
2022-12-11 21:58:12 +0200

    Reorder stuff (2022 day 11 C#)

--- 2022/day-11/Day11Tests.cs
+++ 2022/day-11/Day11Tests.cs
@@ -5,6 +5,42 @@ public class Day11Tests
     private const string Filename = "day11.sample";
 
     [Fact]
+    public void Part1_WithSample_ReturnsMonkeyBusiness()
+    {
+        var monkeyBusiness = Day11.Part1(Filename);
+
+        Assert.Equal(10605, monkeyBusiness);
+    }
+
+    [Fact]
+    public void Part2_WithSample_ReturnsMonkeyBusiness()
+    {
+        var monkeyBusiness = Day11.Part2(Filename);
+
+        Assert.Equal(2713310158, monkeyBusiness);
+    }
+
+    [Fact]
+    public void SimulateMonkeyBusiness_WithSample_Part1_CountsInspections()
+    {
+        var monkeys = Day11.ParseInput(Filename);
+
+        var inspections = Day11.SimulateMonkeyBusiness(monkeys, 20);
+
+        Assert.Equal(new Int64[] { 101, 95, 7, 105 }, inspections);
+    }
+
+    [Fact]
+    public void SimulateMonkeyBusiness_WithSample_Part2_CountsInspections()
+    {
+        var monkeys = Day11.ParseInput(Filename);
+
+        var inspections = Day11.SimulateMonkeyBusiness(monkeys, 10000);
+
+        Assert.Equal(new Int64[] { 52166, 47830, 1938, 52013 }, inspections);
+    }
+
+    [Fact]
     public void ParseInput_WithSampleFile_ReturnsMonkeys()
     {
         var monkeys = Day11.ParseInput(Filename);
@@ -29,41 +65,5 @@ public class Day11Tests
 
         Assert.Equal(new[] { 3, 0, 3, 1
         }, monkeys.Select(monkey => monkey.TargetFalse));
-    }
-
-    [Fact]
-    public void SimulateMonkeyBusiness_WithSample_Part1_CountsInspections()
-    {
-        var monkeys = Day11.ParseInput(Filename);
-
-        var inspections = Day11.SimulateMonkeyBusiness(monkeys, 20);
-
-        Assert.Equal(new Int64[] { 101, 95, 7, 105 }, inspections);
-    }
-
-    [Fact]
-    public void SimulateMonkeyBusiness_WithSample_Part2_CountsInspections()
-    {
-        var monkeys = Day11.ParseInput(Filename);
-
-        var inspections = Day11.SimulateMonkeyBusiness(monkeys, 10000);
-
-        Assert.Equal(new Int64[] { 52166, 47830, 1938, 52013 }, inspections);
-    }
-
-    [Fact]
-    public void Part1_WithSample_ReturnsMonkeyBusiness()
-    {
-        var monkeyBusiness = Day11.Part1(Filename);
-
-        Assert.Equal(10605, monkeyBusiness);
-    }
-
-    [Fact]
-    public void Part2_WithSample_ReturnsMonkeyBusiness()
-    {
-        var monkeyBusiness = Day11.Part2(Filename);
-
-        Assert.Equal(2713310158, monkeyBusiness);
     }
 }
