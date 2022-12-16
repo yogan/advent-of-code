92380de81b20e8b7f87c518382ec8613eed696b2
Frank Blendinger <yogan@zogan.de>
2022-12-16 16:44:04 +0100

    Add notes about PyPy & tqdm to 2022 day 15 README

--- 2022/day-15/run.sh
+++ 2022/day-15/run.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-python3 day15.py
+if command -v pypy3 >/dev/null 2>&1; then
+    pypy3 day15.py
+else
+    echo "pypy3 not found, using python3 instead (slow!)"
+    python3 day15.py
+fi
--- 2022/day-15/sample.sh
+++ 2022/day-15/sample.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-python3 day15.py day15.sample
+if command -v pypy3 >/dev/null 2>&1; then
+    pypy3 day15.py day15.sample
+else
+    echo "pypy3 not found, using python3 instead (slow!)"
+    python3 day15.py day15.sample
+fi
