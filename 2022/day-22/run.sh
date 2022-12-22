06f40b589cd9c3e7be40bd12f55cc3a00700e516
Frank Blendinger <yogan@zogan.de>
2022-12-22 20:57:45 +0100

    Add 2022 day 22 part 1 (Python)

---
+++ 2022/day-22/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ "$(basename "$0")" = "run.sh" ]; then
+    input="input.txt"
+else
+    input="sample.txt"
+fi
+
+# No pypy here, using match which requires Python >= 3.10.
+# pypy3 in Ubuntu 22.04 is only at 3.8
+python3 day*.py "$input"
