402d995c8daa2aad06582adcd303602d2e15fec5
Frank Blendinger <yogan@zogan.de>
2023-11-23 13:10:57 +0100

    Update DDP template to support part 1 and 2 in test

--- templates/ddp/test.sh
+++ templates/ddp/test.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
-result=$(./fuehre-aus.sh | tail -1)
-expected="Das Gesamtvolumen beträgt 14538"
+Ausgabe=$(./fuehre-aus.sh)
 
-if [ "$result" = "$expected" ]; then
-    echo "Test erfolgreich."
-    exit 0
-else
-    echo "Test fehlgeschlagen."
-    echo "Erwarted: $expected"
-    echo "Erhalten: $result"
+Ergebnis1=$(echo "$Ausgabe" | head -1)
+Ergebnis2=$(echo "$Ausgabe" | tail -1)
+
+erwartet1="Das Gesamtvolumen beträgt 14538"
+erwartet2="Das kleinste Volumen ist 484"
+
+if [ "$Ergebnis1" != "$erwartet1" ]; then
+    echo "Test fehlgeschlagen (Teil 1)."
+    echo "Erwartet: »$erwartet1«"
+    echo "Ergebnis: »$Ergebnis1«"
     exit 1
 fi
+
+if [ "$Ergebnis2" != "$erwartet2" ]; then
+    echo "Test fehlgeschlagen (Teil 2)."
+    echo "Erwartet: »$erwartet2«"
+    echo "Ergebnis: »$Ergebnis2«"
+    exit 2
+fi
+
+echo "Test erfolgreich."
