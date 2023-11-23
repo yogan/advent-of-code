b7db8a813813619c19db4584d69993f6a0d3e267
Frank Blendinger <yogan@zogan.de>
2023-11-23 12:56:13 +0100

    Add 2015 day 02 part 1 (DDP)

--- 2015/day-02/test.sh
+++ 2015/day-02/test.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
-result=$(./fuehre-aus.sh | tail -1)
-expected="Es werden 1598415 Quadratfuß Geschenkpapier benötigt."
+Ausgabe=$(./fuehre-aus.sh)
 
-if [ "$result" = "$expected" ]; then
-    echo "Test passed"
-    exit 0
-else
-    echo "Test failed"
-    echo "Expected: $expected"
-    echo "Got: $result"
+Ergebnis1=$(echo "$Ausgabe" | head -1)
+Ergebnis2=$(echo "$Ausgabe" | tail -1)
+
+erwartet1="Es werden 1598415 Quadratfuß Geschenkpapier benötigt."
+erwartet2="Es werden 3812909 Fuß Geschenkband benötigt."
+
+if [ "$Ergebnis1" != "$erwartet1" ]; then
+    echo "Test fehlgeschlagen (Teil 1)."
+    echo "Erwartet: »$erwartet1«"
+    echo "Ergebnis: »$Ergebnis1«"
     exit 1
 fi
+
+if [ "$Ergebnis2" != "$erwartet2" ]; then
+    echo "Test fehlgeschlagen (Teil 2)."
+    echo "Erwartet: »$erwartet2«"
+    echo "Ergebnis: »$Ergebnis2«"
+    exit 2
+fi
+
+echo "Test erfolgreich."
