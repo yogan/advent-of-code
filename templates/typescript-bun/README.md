24764572ac71b70213f84d8864e6e9b3376fa8f5
Frank Blendinger <yogan@zogan.de>
2023-11-28 09:57:25 +0100

    Improve TypeScript (Bun) template (two parts, better tests)
    Former-commit-id: d219a0e5e7a590382d6a13a9c2bca3ba0d231592

---
+++ templates/typescript-bun/README.md
@@ -0,0 +1,8 @@
+# TypeScript (Bun) Advent of Code Template
+
+## Usage
+
+- run: `./run.sh`
+- run (watch mode): `./watch.sh`
+- test: `./test.sh`
+- test (watch mode): `./test-watch.sh`
---
+++ templates/typescript-bun/day-xx.test.ts
@@ -0,0 +1,34 @@
+import { expect, test } from "bun:test"
+import { parseLine, parseInput, part1, part2 } from "./day-xx"
+
+test("parseLine should split the line by 'x'", () => {
+    expect(parseLine("1x2x420")).toEqual([1, 2, 420])
+})
+
+test("parseInput should parse the input file", async () => {
+    const boxes = await parseInput()
+
+    expect(boxes).toHaveLength(5)
+    expect(boxes[0]).toEqual([1, 1, 1])
+    expect(boxes[4]).toEqual([11, 4, 11])
+})
+
+test("part1 should return the sum of the surface areas of the boxes", () => {
+    const b1 = [2, 3, 4] as const
+    const b2 = [1, 1, 10] as const
+    const surface1 = 2 * (2 * 3 + 3 * 4 + 4 * 2)
+    const surface2 = 2 * (1 * 1 + 1 * 10 + 10 * 1)
+    const expected = surface1 + surface2
+
+    expect(part1([b1, b2])).toEqual(expected)
+})
+
+test("part2 should return the sum of the volumes of the boxes", () => {
+    const b1 = [2, 3, 4] as const
+    const b2 = [1, 1, 10] as const
+    const volume1 = 2 * 3 * 4
+    const volume2 = 1 * 1 * 10
+    const expected = volume1 + volume2
+
+    expect(part2([b1, b2])).toEqual(expected)
+})
