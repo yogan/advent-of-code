e5dde5065241f2ae576c165ea0a04ce5884a1806
Frank Blendinger <yogan@zogan.de>
2023-11-29 08:04:59 +0100

    Remove lib from Zig template

--- templates/zig/src/main.zig
+++ templates/zig/src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const testing = std.testing;
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
@@ -17,8 +18,8 @@ pub fn main() !void {
 }
 
 test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
+    var list = std.ArrayList(i32).init(testing.allocator);
     defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
     try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    try testing.expectEqual(@as(i32, 42), list.pop());
 }
