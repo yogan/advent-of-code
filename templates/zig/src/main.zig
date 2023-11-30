5ccc5e6a0efe953ca550a51f7363547efe7322b4
Frank Blendinger <yogan@zogan.de>
2023-11-30 14:53:11 +0100

    Add AoC sample to Zig template
    Former-commit-id: c55f881319cf59d41e74c69ff522381014b8e722

--- templates/zig/src/main.zig
+++ templates/zig/src/main.zig
@@ -1,25 +1,91 @@
 const std = @import("std");
+const stderr = std.debug;
 const testing = std.testing;
+const Allocator = std.mem.Allocator;
+
+// The easiest way to get the input data - just embed it in the binary.
+// Found here: https://www.huy.rocks/everyday/12-11-2022-zig-using-zig-for-advent-of-code#read-input-file
+// The whole article is great, it has a lot of useful hints for advent of code
+// in Zig, without spoiling anything.
+const content = @embedFile("input.txt");
+
+pub fn parseLine(line: []const u8) ![3]u32 {
+    var num_buffer = [3]u32{ 0, 0, 0 };
+    var it = std.mem.split(u8, line, "x");
+    var i: usize = 0;
+    while (it.next()) |num| {
+        const num_u32 = try std.fmt.parseInt(u32, num, 10);
+        num_buffer[i] = num_u32;
+        i += 1;
+    }
+    // stderr.print("num_buffer: {d}\n", .{num_buffer});
+    return num_buffer;
+}
+
+test "parseLine splits line by x and returns array of u32 (single digit numbers)" {
+    const result = try parseLine("1x2x3");
+    try testing.expectEqual(result, .{ 1, 2, 3 });
+}
+
+test "parseLine splits line by x and returns array of u32 (multi digit numbers)" {
+    const result = try parseLine("11x420x1337");
+    try testing.expectEqual(result, .{ 11, 420, 1337 });
+}
+
+fn calc_volume(l: u32, w: u32, h: u32) u32 {
+    return l * w * h;
+}
+
+fn calc_surface_area(l: u32, w: u32, h: u32) u32 {
+    return 2 * l * w + 2 * w * h + 2 * h * l;
+}
+
+fn part1() !u32 {
+    var total_volume: u32 = 0;
+
+    var line_iter = std.mem.split(u8, content, "\n");
+    while (line_iter.next()) |line| {
+        const numbers = try parseLine(line);
+        const volume = calc_volume(numbers[0], numbers[1], numbers[2]);
+        total_volume += volume;
+    }
+
+    return total_volume;
+}
+
+fn part2() !u32 {
+    var total_surface_area: u32 = 0;
+
+    var line_iter = std.mem.split(u8, content, "\n");
+    while (line_iter.next()) |line| {
+        const numbers = try parseLine(line);
+        const surface_area = calc_surface_area(numbers[0], numbers[1], numbers[2]);
+        total_surface_area += surface_area;
+    }
+
+    return total_surface_area;
+}
+
+test "part1 returns correct result" {
+    const result = try part1();
+    try testing.expectEqual(result, 14545);
+}
+
+test "part2 returns correct result" {
+    const result = try part2();
+    try testing.expectEqual(result, 4978);
+}
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const p1 = try part1();
+    const p2 = try part2();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    try stdout.print("Part 1: {d}\n", .{p1});
+    try stdout.print("Part 2: {d}\n", .{p2});
 
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try testing.expectEqual(@as(i32, 42), list.pop());
+    try bw.flush();
 }
