ec805883c37061670ecfaefbe454797af21427e1
Frank Blendinger <yogan@zogan.de>
2023-12-02 19:38:56 +0100

    Enable CI tests for 2023 solutions
    Former-commit-id: c34ee4a66e929b58822e61c66229264f52c63abc

--- 2023/day-02/test-ci.sh
+++ 2023/day-02/test-ci.sh
@@ -1,2 +1,22 @@
 #!/bin/sh
-./test.sh
+
+if ! ./test.sh > /dev/null 2> /dev/null ; then
+    echo "Unit tests failed!"
+    exit 1
+fi
+
+output=$(./run.sh)
+result1=$(echo "$output" | head -1)
+result2=$(echo "$output" | tail -1)
+expected1="Part 1: 2076"
+expected2="Part 2: 70950"
+
+if [ "$result1" != "$expected1" ]; then
+    echo "Expected '$expected1', got '$result1'"
+    exit 1
+fi
+
+if [ "$result2" != "$expected2" ]; then
+    echo "Expected '$expected2', got '$result2'"
+    exit 1
+fi
