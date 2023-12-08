ee53fd706df03ab0e66bfab7a1b1755d8554dace
Frank Blendinger <yogan@zogan.de>
2023-12-08 16:40:15 +0100

    Add Python template
    Closes #9
    
    
    Former-commit-id: 0ee09f19c214506dd4a825e1324c5035de81235e

---
+++ templates/python/dayXX.py
@@ -0,0 +1,55 @@
+import sys, unittest
+
+if len(sys.argv) != 2:
+    print("Missing input file.")
+    sys.exit(1)
+filename  = sys.argv[1]
+sys.argv  = sys.argv[:1] # strip args, they scare the unittest module
+is_sample = filename == "sample.txt"
+
+def parse():
+    with open(filename) as f:
+        lines = [x.strip() for x in f.readlines()]
+    return list(map(parse_line, lines))
+
+def parse_line(line):
+    return [int(x) for x in line.split("x")]
+
+def surface_area(dimensions):
+    l, w, h = dimensions
+    return 2 * (l * w + w * h + h * l)
+
+def volume(dimensions):
+    l, w, h = dimensions
+    return l * w * h
+
+def part1(lines):
+    return sum([volume(x) for x in lines])
+
+def part2(lines):
+    return sum([surface_area(x) for x in lines])
+
+class TestDayXX(unittest.TestCase):
+    def test_parse_line(self):
+        self.assertEqual(parse_line("1x23x420"), [1, 23, 420])
+
+    def test_part1(self):
+        self.assertEqual(part1([[1, 2, 3], [1, 1, 1]]), 6 + 1)
+
+    def test_part2(self):
+        expected = (2 + 2 + 3 + 3 + 6 + 6) + (6 * 1)
+        self.assertEqual(part2([[1, 2, 3], [1, 1, 1]]), expected)
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
+    print()
+
+    lines = parse()
+
+    res1 = part1(lines)
+    assert res1 == (9876 if is_sample else 14545)
+    print(f"Part 1: {res1}{' (sample)' if is_sample else ''}")
+
+    res2 = part2(lines)
+    assert res2 == (21756 if is_sample else 4978)
+    print(f"Part 2: {res2}{' (sample)' if is_sample else ''}")
