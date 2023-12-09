567e1ecc80597a86ce2dc111fd2c57b67948293e
Frank Blendinger <yogan@zogan.de>
2023-12-09 14:47:26 +0100

    Remove inputs from 2015 and 2023, download in CI
    Closes #10
    
    
    Former-commit-id: e6e5a91669aa0342252544468f63c1ea95e9ee02

--- 2015/test.sh
+++ 2015/test.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
-current_directory=$(pwd)
-overall_exit_code=0
+exit_code=0
+cwd=$(pwd)
+year=$(basename "$cwd")
 
 mapfile -t day_dirs < <(find . -mindepth 1 -maxdepth 1 -type d -iname 'day*' -printf '%f\n' | sort)
 
-for dir in "${day_dirs[@]}"; do
-    echo -n "$(basename "$current_directory")/$dir… "
-    if [ ! -f "$dir/test-ci.sh" ]; then
+for day_dir in "${day_dirs[@]}"; do
+    echo -n "$year/$day_dir… "
+
+    if [ ! -f "$day_dir/test-ci.sh" ]; then
         echo "SKIPPED (no test-ci.sh found)"
         continue
     fi
 
-    cd "$dir" || exit 1
+    cd "$day_dir" || exit 1
+
+    if [ ! -f "input.txt" ]; then
+        day=${day_dir//day-/}
+        if ! ../../get-input.sh "$year" "$day" 2>/dev/null; then
+            echo "FAILED (could not download input)"
+            exit_code=1
+            cd "$cwd" || exit 1
+            continue
+        fi
+    fi
+
     if ! ./test-ci.sh; then
         echo "FAILED"
-        overall_exit_code=1
+        exit_code=1
+        cd "$cwd" || exit 1
     else
         echo "OK"
     fi
-    cd "$current_directory" || exit 1
+
+    cd "$cwd" || exit 1
 done
 
-cd "$current_directory" || exit 1
-exit $overall_exit_code
+cd "$cwd" || exit 1
+exit $exit_code
