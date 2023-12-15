93b965be46f56f79c53f95af28fcb171b3bc303d
Frank Blendinger <yogan@zogan.de>
2023-12-15 22:28:57 +0100

    Print output of failed tests for Rust template
    Former-commit-id: 6a3abd3612f38dd25dd31a0f20376fe1b8372a12

--- templates/rust/test-ci.sh
+++ templates/rust/test-ci.sh
@@ -1,4 +1,33 @@
 #!/bin/sh
-./test.sh >/dev/null 2>&1 \
-    && ./sample.sh >/dev/null 2>&1 \
-    && ./run.sh >/dev/null 2>&1
+TEST_LOG="test_log.txt"
+SAMPLE_LOG="sample_log.txt"
+RUN_LOG="run_log.txt"
+
+./test.sh >"$TEST_LOG" 2>&1
+TEST_EXIT_CODE=$?
+
+./sample.sh >"$SAMPLE_LOG" 2>&1
+SAMPLE_EXIT_CODE=$?
+
+./run.sh >"$RUN_LOG" 2>&1
+RUN_EXIT_CODE=$?
+
+if [ $TEST_EXIT_CODE -ne 0 ]; then
+    echo "Tests failed." >&2
+    cat "$TEST_LOG" >&2
+    EXIT_CODE=1
+elif [ $SAMPLE_EXIT_CODE -ne 0 ]; then
+    echo "Run with sample data failed." >&2
+    cat "$SAMPLE_LOG" >&2
+    EXIT_CODE=2
+elif [ $RUN_EXIT_CODE -ne 0 ]; then
+    echo "Run with real input failed." >&2
+    cat "$RUN_LOG" >&2
+    EXIT_CODE=3
+else
+    EXIT_CODE=0
+fi
+
+rm -f "$TEST_LOG" "$SAMPLE_LOG" "$RUN_LOG"
+
+exit $EXIT_CODE
