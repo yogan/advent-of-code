19979d79f76aa4ff91fac255df516a215cc51611
Frank Blendinger <yogan@zogan.de>
2023-12-15 11:12:35 +0100

    Small cleanup for 2023 day 15 (Python)
    Former-commit-id: 161d0466aa88431f87dc458e480398f315e79afd

--- 2023/day-15/day15.py
+++ 2023/day-15/day15.py
@@ -9,19 +9,19 @@ is_sample = filename == "sample.txt"
 def parse():
     return open(filename).read().strip().split(",")
 
-def hash(str):
-    cur = 0
-    for c in str:
-        cur += ord(c)
-        cur *= 17
-        cur &=0xFF
-    return cur
+def hash(string):
+    value = 0
+    for char in string:
+        value += ord(char)
+        value *= 17
+        value %= 256
+    return value
 
 def fill_boxes(input, hashes):
     boxes = [list() for _ in range(256)]
 
     for step in input:
-        if step[-1] == "-":
+        if "-" in step:
             label, focal_length = step[:-1], None
         else:
             label, focal_length = step.split("=")
@@ -29,24 +29,22 @@ def fill_boxes(input, hashes):
         box_idx = hash(label)
 
         if focal_length:
-            labels = [pair[0] for pair in boxes[box_idx]]
+            labels = [label for label, _ in boxes[box_idx]]
             if label in labels:
-                idx = labels.index(label)
-                boxes[box_idx][idx] = (label, focal_length)
+                lens_idx = labels.index(label)
+                boxes[box_idx][lens_idx] = (label, focal_length)
             else:
                 boxes[box_idx].append((label, focal_length))
         else:
-            boxes[box_idx] = [x for x in boxes[box_idx] if x[0] != label]
+            boxes[box_idx] = [(l, f) for l, f in boxes[box_idx] if l != label]
 
     return boxes
 
 def focusing_power(boxes):
     sum = 0
-
-    for box_idx, box in enumerate(boxes):
-        for lens_idx, lens in enumerate(box):
-            sum += (box_idx + 1) * (lens_idx + 1) * int(lens[1])
-
+    for box_idx, box in enumerate(boxes, 1):
+        for lens_idx, (_, focal_length) in enumerate(box, 1):
+            sum += box_idx * lens_idx * int(focal_length)
     return sum
 
 def print_and_assert(part, expected, actual):
@@ -54,12 +52,12 @@ def print_and_assert(part, expected, actual):
     assert actual == expected, f"Part {part} was {actual}, expected {expected}"
 
 if __name__ == '__main__':
-    input = parse()
+    sequence = parse()
 
-    hashes = [hash(str) for str in input]
+    hashes = [hash(step) for step in sequence]
     part1 = sum(hashes)
 
-    boxes = fill_boxes(input, hashes)
+    boxes = fill_boxes(sequence, hashes)
     part2 = focusing_power(boxes)
 
     print_and_assert(1, 1320 if is_sample else 514639, part1)
