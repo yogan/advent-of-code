35f3160197599bb8c9ac393b4cf2851502ad919e
Frank Blendinger <yogan@zogan.de>
2023-12-15 22:36:43 +0100

    Fix rustup "no default toolchain configured" error
    Former-commit-id: 086c80b6293c813c3ddbc6f22e27dea226fdb9ac

--- templates/rust/test-ci.sh
+++ templates/rust/test-ci.sh
@@ -1,33 +1,29 @@
 #!/bin/sh
+
+RUSTUP_LOG="rustup_log.txt"
 TEST_LOG="test_log.txt"
 SAMPLE_LOG="sample_log.txt"
 RUN_LOG="run_log.txt"
 
-./test.sh >"$TEST_LOG" 2>&1
-TEST_EXIT_CODE=$?
+execute_command() {
+    command="$1"
+    log_file="$2"
+    $command >"$log_file" 2>&1
+    exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        echo "Error running \"$command\". Printing log file \"$log_file\":" >&2
+        echo "------------------------------------------------------------" >&2
+        cat "$log_file" >&2
+        exit $exit_code
+    fi
+}
 
-./sample.sh >"$SAMPLE_LOG" 2>&1
-SAMPLE_EXIT_CODE=$?
+# Sometimes rustup complains that no default toolchain is configured.
+# Could be caused by network issues, see: https://stackoverflow.com/a/46864309
+execute_command "rustup default stable" "$RUSTUP_LOG"
 
-./run.sh >"$RUN_LOG" 2>&1
-RUN_EXIT_CODE=$?
+execute_command "./test.sh" "$TEST_LOG"
+execute_command "./sample.sh" "$SAMPLE_LOG"
+execute_command "./run.sh" "$RUN_LOG"
 
-if [ $TEST_EXIT_CODE -ne 0 ]; then
-    echo "Tests failed." >&2
-    cat "$TEST_LOG" >&2
-    EXIT_CODE=1
-elif [ $SAMPLE_EXIT_CODE -ne 0 ]; then
-    echo "Run with sample data failed." >&2
-    cat "$SAMPLE_LOG" >&2
-    EXIT_CODE=2
-elif [ $RUN_EXIT_CODE -ne 0 ]; then
-    echo "Run with real input failed." >&2
-    cat "$RUN_LOG" >&2
-    EXIT_CODE=3
-else
-    EXIT_CODE=0
-fi
-
-rm -f "$TEST_LOG" "$SAMPLE_LOG" "$RUN_LOG"
-
-exit $EXIT_CODE
+rm -f "$RUSTUP_LOG" "$TEST_LOG" "$SAMPLE_LOG" "$RUN_LOG"
