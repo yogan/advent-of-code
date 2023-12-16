6cd2d2673fb42086dd900cc398b505eec897ca52
Frank Blendinger <yogan@zogan.de>
2023-12-16 16:01:54 +0100

    Add optional build step in CI, measure build and run time
    Former-commit-id: a0058d0b44414a5def84fdfe261d9edb7eb2057c

--- 2015/test.sh
+++ 2015/test.sh
@@ -8,7 +8,7 @@ mapfile -t day_dirs < <(find . -mindepth 1 -maxdepth 1 -type d -iname 'day*' -pr
 for day_dir in "${day_dirs[@]}"; do
     echo -n "$year/$day_dir… "
 
-    if [ ! -f "$day_dir/test-ci.sh" ]; then
+    if [ ! -x "$day_dir/test-ci.sh" ]; then
         echo "SKIPPED (no test-ci.sh found)"
         continue
     fi
@@ -25,15 +25,39 @@ for day_dir in "${day_dirs[@]}"; do
         fi
     fi
 
+    # optional build step
+    if [ -x "build-ci.sh" ]; then
+        start=$(date +%s.%N)
+        ./build-ci.sh
+        end=$(date +%s.%N)
+        build_runtime=$(printf "%.0f" "$(echo "($end - $start) * 1000" | bc)")
+        if [ "$build_runtime" -ge 1000 ]; then
+            build_runtime=$(printf "%.1f s" "$(echo "$build_runtime / 1000" | bc -l)")
+        else
+            build_runtime="${build_runtime} ms"
+        fi
+    else
+        build_runtime=""
+    fi
+
     start=$(date +%s.%N)
     if ! ./test-ci.sh; then
         echo "FAILED"
         exit_code=1
-        cd "$cwd" || exit 1
     else
         end=$(date +%s.%N)
-        runtime=$(printf "%4.0f" "$(echo "($end - $start) * 1000" | bc)")
-        echo "OK ($runtime ms)"
+        runtime=$(printf "%.0f" "$(echo "($end - $start) * 1000" | bc)")
+        if [ "$runtime" -ge 1000 ]; then
+            runtime=$(printf "%.1f s" "$(echo "$runtime / 1000" | bc -l)")
+        else
+            runtime="${runtime} ms"
+        fi
+        if [ -n "$build_runtime" ]; then
+            runtime_stats="build: ${build_runtime}, run: ${runtime}"
+        else
+            runtime_stats="${runtime}"
+        fi
+        echo "OK ($runtime_stats)"
     fi
 
     cd "$cwd" || exit 1
--- test-templates.sh
+++ test-templates.sh
@@ -1,23 +1,50 @@
 #!/bin/bash
-current_directory=$(pwd)
-overall_exit_code=0
+exit_code=0
+cwd=$(pwd)
 
 mapfile -t template_dirs < <(find templates/ -mindepth 1 -maxdepth 1 -type d -printf '%f\n' | sort)
 
 for dir in "${template_dirs[@]}"; do
     echo -n "Testing template for $dir… "
     cd "templates/$dir" || exit 1
+
+    # optional build step
+    if [ -x "build-ci.sh" ]; then
+        start=$(date +%s.%N)
+        ./build-ci.sh
+        end=$(date +%s.%N)
+        build_runtime=$(printf "%.0f" "$(echo "($end - $start) * 1000" | bc)")
+        if [ "$build_runtime" -ge 1000 ]; then
+            build_runtime=$(printf "%.1f s" "$(echo "$build_runtime / 1000" | bc -l)")
+        else
+            build_runtime="${build_runtime} ms"
+        fi
+    else
+        build_runtime=""
+    fi
+
     start=$(date +%s.%N)
     if ! ./test-ci.sh; then
         echo "FAILED"
-        overall_exit_code=1
+        exit_code=1
     else
         end=$(date +%s.%N)
         runtime=$(printf "%.0f" "$(echo "($end - $start) * 1000" | bc)")
-        echo "OK ($runtime ms)"
+        if [ "$runtime" -ge 1000 ]; then
+            runtime=$(printf "%.1f s" "$(echo "$runtime / 1000" | bc -l)")
+        else
+            runtime="${runtime} ms"
+        fi
+        if [ -n "$build_runtime" ]; then
+            runtime_stats="build: ${build_runtime}, run: ${runtime}"
+        else
+            runtime_stats="${runtime}"
+        fi
+        echo "OK ($runtime_stats)"
     fi
-    cd "$current_directory" || exit 1
+
+    cd "$cwd" || exit 1
 done
 
-cd "$current_directory" || exit 1
-exit $overall_exit_code
+cd "$cwd" || exit 1
+exit $exit_code
