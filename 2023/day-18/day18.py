c08b4ad701d58ffb72f09c1311e44b430c95a566
Frank Blendinger <yogan@zogan.de>
2023-12-18 15:09:48 +0100

    Rewrite part 1 /w shoelace formula and Pick's theorem
    To prepare for part 2.
    
    
    Former-commit-id: 1fe2c6dd23cddce2aba5ad6d3d476b15b0cbc8a0

--- 2023/day-18/day18.py
+++ 2023/day-18/day18.py
@@ -10,9 +10,7 @@ def parse():
     return [(dir, int(steps), color[2:-1]) for dir, steps, color in
             [line.split() for line in open(filename).readlines()]]
 
-def dig(plan):
-    min_x, min_y = math.inf, math.inf
-    max_x, max_y = 0, 0
+def trace_border(plan):
     pos = (0, 0)
     border = set()
     border.add(pos)
@@ -28,57 +26,43 @@ def dig(plan):
             elif dir == "L":
                 pos = (pos[0] - 1, pos[1])
             else:
-                raise Exception(f"Unknown direction {dir}")
-
-            min_x, min_y = min(min_x, pos[0]), min(min_y, pos[1])
-            max_x, max_y = max(max_x, pos[0]), max(max_y, pos[1])
+                assert False, f"Unknown direction {dir}"
 
             border.add(pos)
 
-    return border, min_x, min_y, max_x, max_y
+    return border
 
-def find_inside(border, min_x, min_y, max_x, max_y, start_x, start_y):
-    seen = set()
-    inside = set()
-    queue = [(start_x, start_y)]
+def get_vertices(plan):
+    pos = (0, 0)
+    vertices = [pos]
 
-    while queue:
-        x, y = queue.pop(0)
+    for dir, steps, _ in plan:
+        if dir == "U":
+            pos = (pos[0], pos[1] - steps)
+        elif dir == "D":
+            pos = (pos[0], pos[1] + steps)
+        elif dir == "R":
+            pos = (pos[0] + steps, pos[1])
+        elif dir == "L":
+            pos = (pos[0] - steps, pos[1])
+        else:
+            assert False, f"Unknown direction {dir}"
 
-        if (x, y) in seen:
-            continue
+        vertices.append(pos)
 
-        seen.add((x, y))
+    assert vertices[0] == vertices[-1], "Path does not end where it started"
 
-        if (x, y) in border:
-            continue
+    return vertices
 
-        inside.add((x, y))
+# https://en.wikipedia.org/wiki/Shoelace_formula
+# https://www.youtube.com/watch?v=0KjG8Pg6LGk
+def shoelace_area(vertices):
+    return abs(sum(x0 * y1 - x1 * y0 for ((x0, y0), (x1, y1)) in
+                   zip(vertices, vertices[1:] + [vertices[0]]))) // 2
 
-        if x < min_x or x > max_x or y < min_y or y > max_y:
-            continue
-
-        queue.append((x - 1, y))
-        queue.append((x + 1, y))
-        queue.append((x, y - 1))
-        queue.append((x, y + 1))
-
-    return inside
-
-def show(border, inside, min_x, min_y, max_x, max_y):
-    for y in range(min_y, max_y + 1):
-        for x in range(min_x, max_x + 1):
-            if (x, y) in inside:
-                assert (x, y) not in border, f"({x}, {y}) is in both inside and border"
-                print("·", end="")
-            elif (x, y) in border:
-                if (x, y) == (0, 0):
-                    print("S", end="")
-                else:
-                    print("#", end="")
-            else:
-                print(" ", end="")
-        print()
+# https://en.wikipedia.org/wiki/Pick%27s_theorem
+def picks_theorem(interior_points, boundary_points):
+    return interior_points + boundary_points // 2 - 1
 
 def print_and_assert(part, expected, actual):
     print(f"Part {part}: {actual}{' (sample)' if is_sample else ''}")
@@ -86,21 +70,11 @@ def print_and_assert(part, expected, actual):
 
 if __name__ == '__main__':
     dig_plan = parse()
-    border, min_x, min_y, max_x, max_y = dig(dig_plan)
 
-    # By looking at the output, we can see that for the sample, below and to the
-    # right of the start position is a valid inside starting position for flood
-    # filling. For my real input, one step to the right is fine.
-    # Alternatively we could just try the flood fill and abort when min/max are
-    # reached while filling, and then try with a different start position, but
-    # this seems excessive.
-    # There must be some clever way to determine the start position…
-    start_x, start_y = (1, 1) if is_sample else (1, 0)
-
-    inside = find_inside(border, min_x, min_y, max_x, max_y, start_x, start_y)
-    # show(border, inside, min_x, min_y, max_x, max_y)
-
-    part1 = len(border) + len(inside)
+    border   = trace_border(dig_plan)
+    vertices = get_vertices(dig_plan)
+    area     = shoelace_area(vertices)
+    part1    = picks_theorem(area, len(border)) + 2 # no idea why + 2…
 
     print_and_assert(1, 62 if is_sample else 92758, part1)
     # print_and_assert(2, 21756 if is_sample else 4978, part2(lines))
