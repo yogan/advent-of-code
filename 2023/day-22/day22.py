6d2894f273349308501b099d20b6cb2110c21772
Frank Blendinger <yogan@zogan.de>
2023-12-22 17:17:59 +0100

    Fancier visualization of the bricks incl. windows
    Former-commit-id: 66f3233c970e7921bd7dc15423f0f6309b38d286

--- 2023/day-22/day22.py
+++ 2023/day-22/day22.py
@@ -1,4 +1,5 @@
 import sys, unittest
+from collections import deque
 
 if len(sys.argv) == 3 and "--visualize" in sys.argv:
     sys.argv.remove("--visualize")
@@ -21,56 +22,153 @@ def parse(file=filename):
     return [to_coords(*line.strip().split("~"))
             for line in open(file).readlines()]
 
-def draw_voxels(blocks):
+def sort_by_z(bricks):
+    return sorted(bricks, key=lambda brick: min(brick[0][2], brick[1][2]))
+
+def get_max_z(bricks):
+    if not bricks:
+        return 1
+    return max(max(brick[0][2], brick[1][2]) for brick in bricks)
+
+def has_collision(x_min, x_max, y_min, y_max, z, bricks):
+    for x in range(x_min, x_max + 1):
+        for y in range(y_min, y_max + 1):
+            for (lx, ly, lz), (rx, ry, rz) in bricks:
+                if (lx <= x <= rx and ly <= y <= ry and lz <= z <= rz):
+                    return True
+    return False
+
+def drop(bricks):
+    settled = []
+    queue = deque(sort_by_z(bricks))
+
+    while queue:
+        (lx, ly, lz), (rx, ry, rz) = queue.popleft()
+
+        x_min, x_max = min(lx, rx), max(lx, rx)
+        y_min, y_max = min(ly, ry), max(ly, ry)
+        z_min        = min(lz, rz)
+
+        z = get_max_z(settled) + 1
+        while z > 1:
+            if has_collision(x_min, x_max, y_min, y_max, z - 1, settled):
+                break
+            z -= 1
+
+        z_diff = z_min - z
+        assert z_diff >= 0, "brick would need to move upwards"
+        settled.append(((lx, ly, lz - z_diff), (rx, ry, rz - z_diff)))
+
+    return settled
+
+def can_be_disintegrated(brick, other_bricks):
+    (lx, ly, lz), (rx, ry, rz) = brick
+
+    x_min, x_max = min(lx, rx), max(lx, rx)
+    y_min, y_max = min(ly, ry), max(ly, ry)
+    z = min(lz, rz) + 1
+
+    above = [b for b in other_bricks
+             if has_collision(x_min, x_max, y_min, y_max, z, [b])]
+
+    # for each brick above, check if we are the *only* supporting brick below
+    for brick_above in above:
+        (alx, aly, alz), (arx, ary, arz) = brick_above
+
+        ax_min, ax_max = min(alx, arx), max(alx, arx)
+        ay_min, ay_max = min(aly, ary), max(aly, ary)
+        az = min(alz, arz) - 1
+
+        below = [b for b in other_bricks
+                 if has_collision(ax_min, ax_max, ay_min, ay_max, az, [b])]
+        if not below:
+            return False
+
+    # print(f"TRUE (brick {brick} can be disintegrated)")
+    return True
+
+def draw_windows(bricks, window_size):
+    step_size = window_size - 1 # to get one plane overlap
+    for z_min in range(1, get_max_z(bricks) + 1, step_size):
+        draw_voxels(bricks, z_min, z_min + step_size)
+
+def draw_voxels(bricks, z_min=1, z_max=None):
     import numpy as np
     import matplotlib.pyplot as plt
     import matplotlib
     from matplotlib.colors import hex2color
     matplotlib.use('TkAgg')
 
-    WIDTH  = max(max(lx, rx) for (lx, _, _), (rx, _, _) in blocks) + 1
-    HEIGHT = max(max(ly, ry) for (_, ly, _), (_, ry, _) in blocks) + 1
-    DEPTH  = max(max(lz, rz) for (_, _, lz), (_, _, rz) in blocks) + 1
+    if z_max is None:
+        z_max = get_max_z(bricks)
 
-    block_colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
+    WIDTH  = max(max(lx, rx) for (lx, _, _), (rx, _, _) in bricks) + 1
+    DEPTH  = max(max(ly, ry) for (_, ly, _), (_, ry, _) in bricks) + 1
+    HEIGHT = max(max(lz, rz) for (_, _, lz), (_, _, rz) in bricks) + 1
+
+    if z_min is not None and z_max is not None:
+        HEIGHT = z_max - z_min + 1
+
+    brick_colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
                     '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
-    colors = np.empty((WIDTH, HEIGHT, DEPTH, 4), dtype=float)
-    voxels = np.zeros((WIDTH, HEIGHT, DEPTH), dtype=bool)
+    colors = np.empty((WIDTH, DEPTH, HEIGHT, 4), dtype=float)
+    voxels = np.zeros((WIDTH, DEPTH, HEIGHT), dtype=bool)
 
-    for i, block in enumerate(blocks, start=1):
-        hex_color = block_colors[i % len(block_colors)]
+    for i, brick in enumerate(bricks, start=1):
+        hex_color = brick_colors[i % len(brick_colors)]
         rgb_color = np.append(hex2color(hex_color), 0.8)
-        for (lx, ly, lz), (rx, ry, rz) in [block]:
+
+        for (lx, ly, lz), (rx, ry, rz) in [brick]:
+
+            if z_min is not None and z_max is not None:
+                # if not (z_min <= lz <= z_max or z_min <= rz <= z_max):
+                #     continue
+                # # shift z values
+                lz -= z_min
+                rz -= z_min
+
             if lx == rx and ly == ry:
+                # pillar (z going up and down)
                 minz, maxz = min(lz, rz), max(lz, rz)
-                colors[lx, ly, minz:maxz + 1] = rgb_color
-                voxels[lx, ly, minz:maxz + 1] = True
-            elif lx == rx and lz == rz:
+                # limit z range even more; we already shifted z down by z_min
+                # z range wihthout shift: z_min..z_max (both inclusive)
+                # z range with shift: 0..z_max - z_min (both inclusive)
+                real_minz = max(0, minz)
+                real_maxz = min(HEIGHT - 1, maxz)
+                colors[lx, ly, real_minz:real_maxz + 1] = rgb_color
+                voxels[lx, ly, real_minz:real_maxz + 1] = True
+            elif lx == rx and lz == rz and 0 <= lz <= HEIGHT - 1:
+                # horizontal plane (y going left and right)
                 miny, maxy = min(ly, ry), max(ly, ry)
                 colors[lx, miny:maxy + 1, lz] = rgb_color
                 voxels[lx, miny:maxy + 1, lz] = True
-            elif ly == ry and lz == rz:
+            elif ly == ry and lz == rz and 0 <= lz <= HEIGHT - 1:
+                # horizontal plane (x going left and right)
                 minx, maxx = min(lx, rx), max(lx, rx)
                 colors[minx:maxx + 1, ly, lz] = rgb_color
                 voxels[minx:maxx + 1, ly, lz] = True
-            else:
-                assert False, "Invalid block"
 
     ax = plt.figure().add_subplot(projection='3d')
     ax.voxels(voxels, facecolors=colors, edgecolor='k')
 
-    ax.set_box_aspect((WIDTH, HEIGHT, DEPTH)) # force cubes to be cubes
-    ax.set_xticklabels([])
-    ax.set_yticklabels([])
-    ax.set_zticklabels([])
+    ax.set_box_aspect((WIDTH, DEPTH, HEIGHT)) # force cubes to be cubes
+
+    ax.set_xticks(range(0,  WIDTH + 1), labels=range(0,  WIDTH + 1))
+    ax.set_yticks(range(0, DEPTH + 1), labels=range(0, DEPTH + 1))
+    ax.set_zticks(range(z_min - z_min, z_max + 2 - z_min),
+                  labels=range(z_min, z_max + 2))
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
 
     plt.show()
 
 class TestDay22(unittest.TestCase):
     def test_parse_sample(self):
-        blocks = parse("sample.txt")
-        self.assertEqual(set(blocks), set([
+        bricks = parse("sample.txt")
+        self.assertEqual(set(bricks), set([
             ((1, 0, 1), (1, 2, 1)),
             ((0, 0, 2), (2, 0, 2)),
             ((0, 2, 3), (2, 2, 3)),
@@ -94,13 +192,25 @@ if __name__ == '__main__':
     unittest.main(exit=False)
     print()
 
-    blocks = parse()
+    bricks = parse()
+
+    # if visualize and is_sample:
+    #     draw_voxels(bricks)
+
+    bricks = drop(bricks)
 
     if visualize:
-        draw_voxels(blocks)
+        draw_windows(bricks, 3 if is_sample else 5)
 
-    part1 = None
+    disintegratable = \
+            [brick for brick in bricks
+             if can_be_disintegrated(brick, [b for b in bricks if b != brick])]
+
+    # print(disintegratable)
+
+    part1 = len(disintegratable)
     part2 = None
 
-    check(1, part1, None if is_sample else None)
+    # 565 too high
+    check(1, part1, 5 if is_sample else None)
     check(2, part2)
