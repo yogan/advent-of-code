42f861d832530c1d685d64db7ddcc7bf3cb39c8c
Frank Blendinger <yogan@zogan.de>
2024-02-22 19:36:54 +0100

    Remove redundant Vim run.sh scripts

--- vim/2023/day-01/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-commented=$(find . -name "*.commented.vim")
-plain=$(echo "$commented" | sed 's/\.commented//g')
-sed -e '/^\("\|$\)/d' <"$commented" >"$plain"
-vim --clean -s "$plain"
-cat out
