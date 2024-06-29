10b568cbedb4658c3279e6bef3c96509b266bb66
Frank Blendinger <yogan@zogan.de>
2024-06-29 13:45:16 +0200

    Generalize Haskell/Stack setup in GitHub Actions

---
+++ scripts/haskell-cleanup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo "Cleaning up ~/.stack directory"
+rm -rf "$HOME/.stack"
---
+++ scripts/haskell-prepare.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# When running in GitHub Actions, $HOME is /home/github, which is owned by
+# uid 1001 / gid 127. We are running as root though, and stack does not like
+# to create its ~/.stack directory in a directory owned by a different user.
+# We can be quicker though, by just creating the directory ourselves. After
+# that, stack will be happy to install its stuff there.
+echo "Creating $HOME/.stack directory as root"
+mkdir -p "$HOME/.stack"
