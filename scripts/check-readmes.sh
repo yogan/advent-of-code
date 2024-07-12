0283f56eb16f357971779bcdb0e5807a9aded487
Frank Blendinger <yogan@zogan.de>
2024-07-12 13:46:09 +0200

    Add config for markdown-link-check

--- scripts/check-readmes.sh
+++ scripts/check-readmes.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+set -x
 set -e pipefail
 
 if command -v fd >/dev/null; then
-    fd README | xargs npx markdown-link-check --quiet --verbose
+    fd_bin=fd
 else
     # Docker image is using fd-find deb from Ubuntu, binary is named fdfind
-    fdfind README | xargs npx markdown-link-check --quiet --verbose
+    fd_bin=fdfind
 fi
+
+$fd_bin README.md | xargs npx markdown-link-check --quiet --verbose \
+    --config .markdown-link-check.config.json
