bca98b2a436783be8e447d5b7a39289f1e73427f
Frank Blendinger <yogan@zogan.de>
2024-08-13 14:05:35 +0200

    Improve Zig template

--- templates/zig/src/main.zig
+++ templates/zig/src/main.zig
@@ -1,91 +1,95 @@
 const std = @import("std");
 const stderr = std.debug;
 const testing = std.testing;
-const Allocator = std.mem.Allocator;
 
-// The easiest way to get the input data - just embed it in the binary.
-// Found here: https://www.huy.rocks/everyday/12-11-2022-zig-using-zig-for-advent-of-code#read-input-file
-// The whole article is great, it has a lot of useful hints for advent of code
-// in Zig, without spoiling anything.
-const content = @embedFile("input.txt");
+const alloc = std.heap.page_allocator;
 
-pub fn parseLine(line: []const u8) ![3]u32 {
-    var num_buffer = [3]u32{ 0, 0, 0 };
+const Box = struct { l: u32, w: u32, h: u32 };
+
+pub fn readInputFile() ![]Box {
+    var file = try std.fs.cwd().openFile("input.txt", .{});
+    defer file.close();
+
+    var buf: [1024]u8 = undefined;
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
+    var parsedLines = std.ArrayList(Box).init(alloc);
+
+    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        try parsedLines.append(try parseLine(line));
+    }
+
+    return parsedLines.items;
+}
+
+pub fn parseLine(line: []const u8) !Box {
+    var buf = [3]u32{ 0, 0, 0 };
     var it = std.mem.split(u8, line, "x");
     var i: usize = 0;
     while (it.next()) |num| {
         const num_u32 = try std.fmt.parseInt(u32, num, 10);
-        num_buffer[i] = num_u32;
+        buf[i] = num_u32;
         i += 1;
     }
-    // stderr.print("num_buffer: {d}\n", .{num_buffer});
-    return num_buffer;
+    return Box{ .l = buf[0], .w = buf[1], .h = buf[2] };
 }
 
-test "parseLine splits line by x and returns array of u32 (single digit numbers)" {
+test "parseLine splits line by x and returns a box (single digit numbers)" {
     const result = try parseLine("1x2x3");
-    try testing.expectEqual(result, .{ 1, 2, 3 });
+    try testing.expectEqual(result, Box{ .l = 1, .w = 2, .h = 3 });
 }
 
-test "parseLine splits line by x and returns array of u32 (multi digit numbers)" {
+test "parseLine splits line by x and returns a box (multi digit numbers)" {
     const result = try parseLine("11x420x1337");
-    try testing.expectEqual(result, .{ 11, 420, 1337 });
+    try testing.expectEqual(result, Box{ .l = 11, .w = 420, .h = 1337 });
 }
 
-fn calc_volume(l: u32, w: u32, h: u32) u32 {
-    return l * w * h;
+fn calc_volume(box: Box) u32 {
+    return box.l * box.w * box.h;
 }
 
-fn calc_surface_area(l: u32, w: u32, h: u32) u32 {
-    return 2 * l * w + 2 * w * h + 2 * h * l;
+fn calc_surface_area(box: Box) u32 {
+    return 2 * (box.l * box.w + box.w * box.h + box.h * box.l);
 }
 
-fn part1() !u32 {
-    var total_volume: u32 = 0;
-
-    var line_iter = std.mem.split(u8, content, "\n");
-    while (line_iter.next()) |line| {
-        const numbers = try parseLine(line);
-        const volume = calc_volume(numbers[0], numbers[1], numbers[2]);
-        total_volume += volume;
+fn part1(boxes: []const Box) !u32 {
+    var sum: u32 = 0;
+    for (boxes) |box| {
+        sum += calc_volume(box);
     }
-
-    return total_volume;
+    return sum;
 }
 
-fn part2() !u32 {
-    var total_surface_area: u32 = 0;
-
-    var line_iter = std.mem.split(u8, content, "\n");
-    while (line_iter.next()) |line| {
-        const numbers = try parseLine(line);
-        const surface_area = calc_surface_area(numbers[0], numbers[1], numbers[2]);
-        total_surface_area += surface_area;
+fn part2(boxes: []const Box) !u32 {
+    var sum: u32 = 0;
+    for (boxes) |box| {
+        sum += calc_surface_area(box);
     }
-
-    return total_surface_area;
+    return sum;
 }
 
-test "part1 returns correct result" {
-    const result = try part1();
-    try testing.expectEqual(result, 14545);
+test "part1 returns sum of volumes of boxes" {
+    const boxes = [_]Box{ Box{ .l = 2, .w = 3, .h = 4 }, Box{ .l = 1, .w = 1, .h = 10 } };
+    const expected = (2 * 3 * 4) + (1 * 1 * 10);
+
+    try testing.expectEqual(try part1(&boxes), expected);
 }
 
-test "part2 returns correct result" {
-    const result = try part2();
-    try testing.expectEqual(result, 4978);
+test "part2 returns sum of surface areas of boxes" {
+    const boxes = [_]Box{ Box{ .l = 2, .w = 3, .h = 4 }, Box{ .l = 1, .w = 1, .h = 10 } };
+    const expected = 2 * (2 * 3 + 3 * 4 + 4 * 2) + 2 * (1 * 1 + 1 * 10 + 10 * 1);
+
+    try testing.expectEqual(try part2(&boxes), expected);
 }
 
 pub fn main() !void {
-    const p1 = try part1();
-    const p2 = try part2();
-
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("{d}\n", .{p1});
-    try stdout.print("{d}\n", .{p2});
+    const boxes = try readInputFile();
+    try stdout.print("{d}\n", .{try part1(boxes)});
+    try stdout.print("{d}\n", .{try part2(boxes)});
 
     try bw.flush();
 }
