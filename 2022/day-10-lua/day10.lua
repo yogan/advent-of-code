c9c90381c4e339dffcf21d9a2a6cb779e8dac19b
Frank Blendinger <yogan@zogan.de>
2024-09-09 17:39:21 +0200

    Fix Lua LSP warnings, auto format (2022 Day 10 Lua)

--- 2022/day-10-lua/day10.lua
+++ 2022/day-10-lua/day10.lua
@@ -1,89 +1,90 @@
 local day10 = {}
 
-function replace_char(pos, str, c)
-    return str:sub(1, pos - 1) .. c .. str:sub(pos + 1)
+local function replace_char(pos, str, c)
+	return str:sub(1, pos - 1) .. c .. str:sub(pos + 1)
 end
 
 function string.insert(str1, str2, pos)
-    return str1:sub(1, pos) .. str2 .. str1:sub(pos + 1)
+	return str1:sub(1, pos) .. str2 .. str1:sub(pos + 1)
 end
 
-function format(crt)
-    for i = 5, 1, -1 do
-        crt = string.insert(crt, "\n", 40 * i)
-    end
-    return crt
+local function format(crt)
+	for i = 5, 1, -1 do
+		crt = string.insert(crt, "\n", 40 * i)
+	end
+	return crt
 end
 
-function sum(values)
-    local sum = 0
-    for _, value in ipairs(values) do
-        sum = sum + value
-    end
-    return sum
+local function sum(values)
+	local res = 0
+	for _, value in ipairs(values) do
+		res = res + value
+	end
+	return res
 end
 
-function update_crt(crt, cycle, sprite_pos)
-    local pos = cycle % (40 * 6) + 1
-    local line_pos = cycle % 40 + 1
-    if (line_pos == sprite_pos or line_pos == sprite_pos + 1 or line_pos == sprite_pos + 2) then
-        return replace_char(pos, crt, "#")
-    end
-    return crt
+local function update_crt(crt, cycle, sprite_pos)
+	local pos = cycle % (40 * 6) + 1
+	local line_pos = cycle % 40 + 1
+	if sprite_pos <= line_pos and line_pos <= sprite_pos + 2 then
+		return replace_char(pos, crt, "#")
+	end
+	return crt
 end
 
-function day10.parseInput(filename)
-    local file = io.open(filename, "r")
-    local cycle = 0
-    local x = 1
-    local signalStrengths = {}
+function day10.parse_input(filename)
+	local file = io.open(filename, "r")
+	local cycle = 0
+	local x = 1
+	local signal_strengths = {}
 
-    local interesting = {
-        [20] = true,
-        [60] = true,
-        [100] = true,
-        [140] = true,
-        [180] = true,
-        [220] = true
-    }
+	local interesting = {
+		[20] = true,
+		[60] = true,
+		[100] = true,
+		[140] = true,
+		[180] = true,
+		[220] = true,
+	}
 
-    local crt = string.rep(".", 40 * 6)
+	local crt = string.rep(".", 40 * 6)
 
-    for line in file:lines() do
-        if (line == "noop") then
-            crt = update_crt(crt, cycle, x)
-            cycle = cycle + 1
-            if (interesting[cycle]) then
-                table.insert(signalStrengths, cycle * x)
-            end
-        else
-            crt = update_crt(crt, cycle, x)
-            cycle = cycle + 1
-            if (interesting[cycle]) then
-                table.insert(signalStrengths, cycle * x)
-            end
-            crt = update_crt(crt, cycle, x)
-            cycle = cycle + 1
-            if (interesting[cycle]) then
-                table.insert(signalStrengths, cycle * x)
-            end
-            for op, num in string.gmatch(line, "(addx) (.+)") do
-                assert(op == "addx")
-                x = x + tonumber(num)
-            end
-        end
-    end
+	---@diagnostic disable-next-line: need-check-nil
+	for line in file:lines() do
+		if line == "noop" then
+			crt = update_crt(crt, cycle, x)
+			cycle = cycle + 1
+			if interesting[cycle] then
+				table.insert(signal_strengths, cycle * x)
+			end
+		else
+			crt = update_crt(crt, cycle, x)
+			cycle = cycle + 1
+			if interesting[cycle] then
+				table.insert(signal_strengths, cycle * x)
+			end
+			crt = update_crt(crt, cycle, x)
+			cycle = cycle + 1
+			if interesting[cycle] then
+				table.insert(signal_strengths, cycle * x)
+			end
+			for op, num in string.gmatch(line, "(addx) (.+)") do
+				assert(op == "addx")
+				x = x + tonumber(num)
+			end
+		end
+	end
 
-    return {
-        cycle = cycle,
-        x = x,
-        signalStrengths = signalStrengths,
-        sum = sum(signalStrengths),
-        crt = format(crt)
-    }
+	return {
+		cycle = cycle,
+		x = x,
+		signal_strengths = signal_strengths,
+		sum = sum(signal_strengths),
+		crt = format(crt),
+	}
 end
 
-local result = day10.parseInput("day10.in")
+local result = day10.parse_input("day10.in")
 print("Part 1:", result.sum)
 print("Part 2:")
 print(result.crt)
--- 2022/day-10-lua/day10_spec.lua
+++ 2022/day-10-lua/day10_spec.lua
@@ -1,25 +1,30 @@
-local day10 = require "day10"
+local day10 = require("day10")
 
 describe("Advent of Code 2022 Day 10", function()
-    it("should calculate the signal strengths for the sample input", function()
-        local result = day10.parseInput("day10.sample")
-        assert.are.same({420, 1140, 1800, 2940, 2880, 3960}, result.signalStrengths)
-    end)
+	describe("parse_input", function()
+		it("should calculate the signal strengths for the sample input", function()
+			local result = day10.parse_input("day10.sample").signal_strengths
+			assert.are.same({ 420, 1140, 1800, 2940, 2880, 3960 }, result)
+		end)
 
-    it("should return the signal strength sum the sample input", function()
-        local result = day10.parseInput("day10.sample")
-        assert.are.same(13140, result.sum)
-    end)
+		it("should return the signal strength sum the sample input", function()
+			local result = day10.parse_input("day10.sample")
+			assert.are.same(13140, result.sum)
+		end)
 
-    it("should draw the final CRT image for the sample input", function()
-        local result = day10.parseInput("day10.sample")
-        assert.are.same(table.concat({
-            "##..##..##..##..##..##..##..##..##..##..",
-            "###...###...###...###...###...###...###.",
-            "####....####....####....####....####....",
-            "#####.....#####.....#####.....#####.....",
-            "######......######......######......####",
-            "#######.......#######.......#######....."
-        }, "\n"), result.crt)
-    end)
+		it("should draw the final CRT image for the sample input", function()
+			local result = day10.parse_input("day10.sample")
+			assert.are.same(
+				table.concat({
+					"##..##..##..##..##..##..##..##..##..##..",
+					"###...###...###...###...###...###...###.",
+					"####....####....####....####....####....",
+					"#####.....#####.....#####.....#####.....",
+					"######......######......######......####",
+					"#######.......#######.......#######.....",
+				}, "\n"),
+				result.crt
+			)
+		end)
+	end)
 end)
