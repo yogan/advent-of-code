01456d87c42ba6ed1adccf07b32613a50ce3fbfa
Frank Blendinger <yogan@zogan.de>
2024-12-02 10:10:39 +0100

    Add 2024 day 02 part 1 in Python

---
+++ 2024/day-02-python/aoc.py
@@ -0,0 +1,66 @@
+import sys
+import unittest
+
+
+def parse():
+    return [
+        [int(num) for num in line.strip().split()]
+        for line in open(filename).readlines()
+    ]
+
+
+def count_safe_reports(reports):
+    return sum([1 for report in reports if is_safe(report)])
+
+
+def is_safe(levels):
+    diffs = [b - a for a, b in zip(levels, levels[1:])]
+    signs = [d > 0 for d in diffs]
+
+    all_increasing = all(signs)
+    all_decreasing = all(not i for i in signs)
+
+    diffs_in_range = all([1 <= abs(d) <= 3 for d in diffs])
+
+    return (all_increasing or all_decreasing) and diffs_in_range
+
+
+class Tests(unittest.TestCase):
+    def test_is_safe(self):
+        self.assertEqual(is_safe([7, 6, 4, 2, 1]), True)
+        self.assertEqual(is_safe([1, 2, 7, 8, 9]), False)
+        self.assertEqual(is_safe([9, 7, 6, 2, 1]), False)
+        self.assertEqual(is_safe([1, 3, 2, 4, 5]), False)
+        self.assertEqual(is_safe([8, 6, 4, 4, 1]), False)
+        self.assertEqual(is_safe([1, 3, 6, 7, 9]), True)
+
+
+if __name__ == "__main__":
+    flags = set(arg for arg in sys.argv[1:] if arg.startswith("-"))
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
+    sys.argv = sys.argv[:1]  # strip args, unittest.main() doesn't like them
+
+    filename = "sample.txt" if "-s" in flags or "--sample" in flags else "input.txt"
+    filename = args[0] if args else filename
+    is_sample = filename.startswith("sample")
+    run_tests = "-t" in flags or "--test" in flags
+
+    if run_tests:
+        unittest.main(exit=True)
+
+    def check(part, actual, expected=None):
+        print(f"Part {part}{' (sample)' if is_sample else ''}: {actual} ", end="")
+        if expected is None:
+            print("❔")
+        else:
+            if actual != expected:
+                print(f"≠ {expected} ❌")
+                exit(1)
+            print("✅")
+
+    reports = parse()
+    part1 = count_safe_reports(reports)
+    part2 = None
+
+    check(1, part1, 2 if is_sample else 252)
+    check(2, part2)
