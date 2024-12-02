8901a53e30494309d908a882afe5e7af941c88ee
Frank Blendinger <yogan@zogan.de>
2024-12-02 10:26:32 +0100

    Add 2024 day 02 part 2 in Python

--- 2024/day-02-python/aoc.py
+++ 2024/day-02-python/aoc.py
@@ -9,20 +9,27 @@ def parse():
     ]
 
 
-def count_safe_reports(reports):
-    return sum([1 for report in reports if is_safe(report)])
-
-
 def is_safe(levels):
     diffs = [b - a for a, b in zip(levels, levels[1:])]
     signs = [d > 0 for d in diffs]
 
     all_increasing = all(signs)
     all_decreasing = all(not i for i in signs)
-
     diffs_in_range = all([1 <= abs(d) <= 3 for d in diffs])
 
     return (all_increasing or all_decreasing) and diffs_in_range
+
+
+def is_safe_enough(levels):
+    if is_safe(levels):
+        return True
+
+    for i in range(0, len(levels)):
+        levels_without_i = levels[:i] + levels[i + 1 :]
+        if is_safe(levels_without_i):
+            return True
+
+    return False
 
 
 class Tests(unittest.TestCase):
@@ -33,6 +40,14 @@ class Tests(unittest.TestCase):
         self.assertEqual(is_safe([1, 3, 2, 4, 5]), False)
         self.assertEqual(is_safe([8, 6, 4, 4, 1]), False)
         self.assertEqual(is_safe([1, 3, 6, 7, 9]), True)
+
+    def test_is_safe_enough(self):
+        self.assertEqual(is_safe_enough([7, 6, 4, 2, 1]), True)
+        self.assertEqual(is_safe_enough([1, 2, 7, 8, 9]), False)
+        self.assertEqual(is_safe_enough([9, 7, 6, 2, 1]), False)
+        self.assertEqual(is_safe_enough([1, 3, 2, 4, 5]), True)
+        self.assertEqual(is_safe_enough([8, 6, 4, 4, 1]), True)
+        self.assertEqual(is_safe_enough([1, 3, 6, 7, 9]), True)
 
 
 if __name__ == "__main__":
@@ -59,8 +74,8 @@ if __name__ == "__main__":
             print("✅")
 
     reports = parse()
-    part1 = count_safe_reports(reports)
-    part2 = None
+    part1 = sum(1 for report in reports if is_safe(report))
+    part2 = sum(1 for report in reports if is_safe_enough(report))
 
     check(1, part1, 2 if is_sample else 252)
-    check(2, part2)
+    check(2, part2, 4 if is_sample else 324)
