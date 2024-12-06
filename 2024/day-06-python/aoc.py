370f2d5e625fc7a1f8f71999050294cb346d02f4
Frank Blendinger <yogan@zogan.de>
2024-12-06 08:01:49 +0100

    Clean up 2024 day 06 Python solution

--- 2024/day-06-python/aoc.py
+++ 2024/day-06-python/aoc.py
@@ -3,67 +3,30 @@ import unittest
 
 
 def parse():
-    return [list(line.strip()) for line in open(filename).readlines()]
+    return list(map(list, (open(filename).read().split())))
 
 
-def travel(lines):
-    X_MAX = len(lines[0])
-    Y_MAX = len(lines)
-    x, y = find_guard(lines)
-    visited = {(x, y)}
-
-    while True:
-        x_next, y_next = next_pos((x, y), lines[y][x])
-        if x_next < 0 or x_next >= X_MAX or y_next < 0 or y_next >= Y_MAX:
-            break
-        if lines[y_next][x_next] == "#":
-            lines[y][x] = rotate_right(lines[y][x])
-        else:
-            lines[y_next][x_next] = lines[y][x]
-            lines[y][x] = "."
-            x, y = x_next, y_next
-            visited.add((x, y))
-
-    return visited
-
-
-def has_loop(lines, guard_start):
-    X_MAX = len(lines[0])
-    Y_MAX = len(lines)
+def travel(lab, guard_start):
+    X_MAX = len(lab[0])
+    Y_MAX = len(lab)
     x, y = guard_start
-    visited = {(x, y, lines[y][x])}
+    visited = {(x, y, lab[y][x])}
 
     while True:
-        x_next, y_next = next_pos((x, y), lines[y][x])
+        x_next, y_next = next_pos((x, y), lab[y][x])
         if x_next < 0 or x_next >= X_MAX or y_next < 0 or y_next >= Y_MAX:
             break
-        if lines[y_next][x_next] == "#":
-            lines[y][x] = rotate_right(lines[y][x])
+        if lab[y_next][x_next] == "#":
+            lab[y][x] = rotate_right(lab[y][x])
         else:
-            lines[y_next][x_next] = lines[y][x]
-            lines[y][x] = "."
+            lab[y_next][x_next] = lab[y][x]
+            lab[y][x] = "."
             x, y = x_next, y_next
-            if (x, y, lines[y][x]) in visited:
-                return True
-            visited.add((x, y, lines[y][x]))
+            if (x, y, lab[y][x]) in visited:
+                return (True, {})
+            visited.add((x, y, lab[y][x]))
 
-    return False
-
-
-def part2(lines, visited):
-    guard_start = find_guard(lines)
-    candidates = [pos for pos in visited if pos != guard_start]
-
-    loops = 0
-
-    while candidates:
-        x, y = candidates.pop()
-        lines_copy = [line.copy() for line in lines]
-        lines_copy[y][x] = "#"
-        if has_loop(lines_copy, guard_start):
-            loops += 1
-
-    return loops
+    return (False, {(x, y) for x, y, _ in visited})
 
 
 def next_pos(pos, direction):
@@ -91,35 +54,46 @@ def rotate_right(direction):
     raise ValueError(f"Invalid direction: {direction}")
 
 
-def find_guard(lines):
-    for y, line in enumerate(lines):
-        for x, c in enumerate(line):
+def find_guard(lab):
+    for y, row in enumerate(lab):
+        for x, c in enumerate(row):
             if c == "^":
                 return (x, y)
     raise ValueError("Guard not found")
 
 
+def part2(lab, visited):
+    guard_start = find_guard(lab)
+    candidates = [pos for pos in visited if pos != guard_start]
+
+    return sum(loops(lab, pos, guard_start) for pos in candidates)
+
+
+def loops(lab, pos, guard_start):
+    x, y = pos
+    lab_copy = [row.copy() for row in lab]
+    lab_copy[y][x] = "#"
+
+    return travel(lab_copy, guard_start)[0]
+
+
 class Tests(unittest.TestCase):
     def test_travel(self):
-        self.assertEqual(
-            len(
-                travel(
-                    [
-                        list("....#....."),
-                        list(".........#"),
-                        list(".........."),
-                        list("..#......."),
-                        list(".......#.."),
-                        list(".........."),
-                        list(".#..^....."),
-                        list("........#."),
-                        list("#........."),
-                        list("......#..."),
-                    ]
-                )
-            ),
-            41,
-        )
+        lab = [
+            list("....#....."),
+            list(".........#"),
+            list(".........."),
+            list("..#......."),
+            list(".......#.."),
+            list(".........."),
+            list(".#..^....."),
+            list("........#."),
+            list("#........."),
+            list("......#..."),
+        ]
+        loops, visited = travel(lab, find_guard(lab))
+        self.assertEqual(loops, False)
+        self.assertEqual(len(visited), 41)
 
 
 if __name__ == "__main__":
@@ -145,10 +119,10 @@ if __name__ == "__main__":
                 exit(1)
             print("✅")
 
-    lines = parse()
-    visited = travel([line.copy() for line in lines])
+    lab = parse()
+    _, visited = travel([row.copy() for row in lab], find_guard(lab))
     p1 = len(visited)
-    p2 = part2(lines, visited)
+    p2 = part2(lab, visited)
 
     check(1, p1, 41 if is_sample else 4973)
     check(2, p2, 6 if is_sample else 1482)
