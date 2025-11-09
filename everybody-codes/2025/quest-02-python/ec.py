71bef6158a16ba725c143ed6f995fdfcda0266b4
Frank Blendinger <yogan@zogan.de>
2025-11-09 10:30:07 +0100

    Add EC2025 Quest 2 Part 2 (Python)

--- everybody-codes/2025/quest-02-python/ec.py
+++ everybody-codes/2025/quest-02-python/ec.py
@@ -16,6 +16,33 @@ def part1(a):
     return r
 
 
+def part2(a):
+    ax, ay = a
+    count, step, dim = 0, 10, 1001
+
+    for y in range(ay, ay + dim, step):
+        for x in range(ax, ax + dim, step):
+            if engrave((x, y)):
+                count += 1
+
+    return count
+
+
+def engrave(coord):
+    val = (0, 0)
+    limit = 1_000_000
+
+    for _ in range(100):
+        val = mult(val, val)
+        val = div(val, (100_000, 100_000))
+        val = add(val, coord)
+        x, y = val
+        if x > limit or x < -limit or y > limit or y < -limit:
+            return False
+
+    return True
+
+
 def add(a, b):
     ax, ay = a
     bx, by = b
@@ -31,7 +58,7 @@ def mult(a, b):
 def div(a, b):
     ax, ay = a
     bx, by = b
-    return ax // bx, ay // by
+    return int(ax / bx), int(ay / by)
 
 
 def to_str(a):
@@ -48,6 +75,19 @@ class Tests(unittest.TestCase):
 
     def test_div(self):
         self.assertEqual(div((-10, -12), (2, 2)), (-5, -6))
+
+    def test_engrave(self):
+        self.assertTrue(engrave((35630, -64880)))
+        self.assertTrue(engrave((35630, -64870)))
+        self.assertTrue(engrave((35640, -64860)))
+        self.assertTrue(engrave((36230, -64270)))
+        self.assertTrue(engrave((36250, -64270)))
+
+        self.assertFalse(engrave((35460, -64910)))
+        self.assertFalse(engrave((35470, -64910)))
+        self.assertFalse(engrave((35480, -64910)))
+        self.assertFalse(engrave((35680, -64850)))
+        self.assertFalse(engrave((35630, -64830)))
 
 
 if __name__ == "__main__":
@@ -73,5 +113,7 @@ if __name__ == "__main__":
 
     if is_sample:
         check(1, to_str(part1(parse("sample1.txt"))), "[357,862]")
+        check(2, part2(parse("sample2.txt")), 4076)
     else:
         check(1, to_str(part1(parse("input1.txt"))), "[483530,983550]")
+        check(2, part2(parse("input2.txt")), 632)
