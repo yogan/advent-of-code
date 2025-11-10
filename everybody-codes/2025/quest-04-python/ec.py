a9a27ced1b0bb99d6aade59ef328b4aabeed273a
Frank Blendinger <yogan@zogan.de>
2025-11-10 10:27:58 +0100

    Add EC2025 Quest 4 Part 3 (Python)

--- everybody-codes/2025/quest-04-python/ec.py
+++ everybody-codes/2025/quest-04-python/ec.py
@@ -3,7 +3,7 @@ import unittest
 
 
 def parse(filename):
-    return list(map(int, open(filename).readlines()))
+    return list(map(int, open(filename).read().strip().replace("|", "\n").split("\n")))
 
 
 def part1(xs):
@@ -12,6 +12,13 @@ def part1(xs):
 
 def part2(xs):
     return round(10_000_000_000_000 / factor(xs))
+
+
+def part3(xs):
+    val = 100
+    for i in range(0, len(xs) - 1, 2):
+        val *= xs[i] / xs[i + 1]
+    return int(val)
 
 
 def factor(xs):
@@ -26,6 +33,10 @@ class Tests(unittest.TestCase):
         self.assertEqual(part1([128, 64, 32, 16, 8]), 32400)
         self.assertEqual(part1([102, 75, 50, 35, 13]), 15888)
 
+    def test_part3(self):
+        self.assertEqual(part3([5, 5, 10, 10, 20, 5]), 400)
+        self.assertEqual(part3([5, 7, 21, 18, 36, 27, 27, 10, 50, 10, 50, 11]), 6818)
+
 
 def main():
     failures = 0
@@ -35,9 +46,11 @@ def main():
         failures += check(1.2, part1(parse("sample2.txt")), 15888)
         failures += check(2.1, part2(parse("sample1.txt")), 625_000_000_000)
         failures += check(2.2, part2(parse("sample2.txt")), 1_274_509_803_922)
+        failures += check(3, part3(parse("sample3.txt")), 6818)
     else:
         failures += check(1, part1(parse("input1.txt")), 11773)
         failures += check(2, part2(parse("input2.txt")), 2_631_016_042_781)
+        failures += check(3, part3(parse("input3.txt")), 660_846_420_232)
 
     exit(failures)
 
