0f63d7dde8156d0ccef32fd389f8a4db358fa142
Frank Blendinger <yogan@zogan.de>
2025-11-11 13:44:26 +0100

    Add EC2025 Quest 6 Part 3 (Python)

--- everybody-codes/2025/quest-06-python/ec.py
+++ everybody-codes/2025/quest-06-python/ec.py
@@ -18,6 +18,10 @@ def part2(letters):
     )
 
 
+def part3(letters):
+    return count_pairs_repeating(letters, 1000, 1000)
+
+
 def count_pairs(letters, char):
     letters = [c for c in letters if c.lower() == char]
     pairs = 0
@@ -29,12 +33,30 @@ def count_pairs(letters, char):
     return pairs
 
 
+def count_pairs_repeating(letters, reps, dist):
+    letters = letters * reps
+    pairs = 0
+
+    for i, c in enumerate(letters):
+        if c.islower():
+            left = letters[max(0, i - dist) : min(len(letters), i + dist + 1)]
+            pairs += len([x for x in left if x == c.upper()])
+
+    return pairs
+
+
 class Tests(unittest.TestCase):
     def test_part1(self):
         self.assertEqual(part1(list("ABabACacBCbca")), 5)
 
     def test_part2(self):
         self.assertEqual(part2(list("ABabACacBCbca")), 11)
+
+    def test_count_pairs_repeating(self):
+        letters = list("AABCBABCABCabcabcABCCBAACBCa")
+        self.assertEqual(count_pairs_repeating(letters, 1, 10), 34)
+        self.assertEqual(count_pairs_repeating(letters, 2, 10), 72)
+        self.assertEqual(count_pairs_repeating(letters, 1_000, 1_000), 3_442_321)
 
 
 def main():
@@ -43,9 +65,11 @@ def main():
     if is_sample:
         failures += check(1, part1(parse("sample1.txt")), 5)
         failures += check(2, part2(parse("sample2.txt")), 11)
+        failures += check(3, part3(parse("sample3.txt")), 3_442_321)
     else:
         failures += check(1, part1(parse("input1.txt")), 151)
-        failures += check(2, part2(parse("input2.txt")), 3747)
+        failures += check(2, part2(parse("input2.txt")), 3_747)
+        failures += check(3, part3(parse("input3.txt")), 1_667_390_009)
 
     exit(failures)
 
