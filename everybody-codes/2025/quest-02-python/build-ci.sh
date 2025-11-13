b767197ec2d8f8f233cfab9f89f5a8415d9ec44e
Frank Blendinger <yogan@zogan.de>
2025-11-13 17:09:57 +0100

    Disable shellcheck SC1091 (not following sourced files)

--- everybody-codes/2025/quest-02-python/build-ci.sh
+++ everybody-codes/2025/quest-02-python/build-ci.sh
@@ -8,7 +8,6 @@ cd "$(dirname "$0")" || exit 1
 
 if [ -f requirements.txt ]; then
     pypy3 -m venv venv
-    # shellcheck disable=SC1091
     . venv/bin/activate
     pypy3 -m pip install -r requirements.txt
 fi
