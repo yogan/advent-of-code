d0c2731717ce8d1b6d3bfed5cf6d5d7d1257eb2e
Frank Blendinger <yogan@zogan.de>
2025-11-13 21:23:48 +0100

    Simplify EC2025 Quest 8 Part 2 (Python)

--- everybody-codes/2025/quest-08-python/ec.py
+++ everybody-codes/2025/quest-08-python/ec.py
@@ -1,6 +1,5 @@
 import sys
 import unittest
-from collections import defaultdict
 
 
 def parse(filename):
@@ -17,48 +16,39 @@ def part1(xs, nails):
     return total
 
 
-def part2(xs, nails):
+def part2(xs):
     total = 0
-    lines = defaultdict(int)
+    lines = set()
 
     for a, b in zip(xs, xs[1:]):
         a, b = min(a, b), max(a, b)
-        intersections = [x for x in lines.keys() if intersect((a, b), x)]
-        total += len(intersections)
-        lines[(a, b)] += 1
+        total += sum([1 for c, d in lines if intersect(a, b, c, d)])
+        lines.add((a, b))
 
     return total
 
 
-def intersect(line1, line2):
-    a, b = line1
-    c, d = line2
-
-    # to intersect, the end points of both lines have to alternate
+def intersect(a, b, c, d):
+    # when going around the circle, the points of both lines have to alternate
     return a < c < b < d or c < a < d < b
 
 
 class Tests(unittest.TestCase):
-    nails = 8
-
     def test_part1(self):
-        self.assertEqual(part1([1, 5, 2, 6, 8, 4, 1, 7, 3], self.nails), 4)
+        self.assertEqual(part1([1, 5, 2, 6, 8, 4, 1, 7, 3], 8), 4)
 
     def test_part2(self):
-        self.assertEqual(part2([1, 5, 2, 6, 8, 4, 1, 7, 3, 5, 7, 8, 2], self.nails), 21)
+        self.assertEqual(part2([1, 5, 2, 6, 8, 4, 1, 7, 3, 5, 7, 8, 2]), 21)
 
     def test_intersect(self):
-        self.assertFalse(intersect((1, 2), (2, 3)))
-        self.assertFalse(intersect((2, 3), (1, 2)))
-
-        self.assertFalse(intersect((1, 5), (2, 5)))
-        self.assertFalse(intersect((2, 5), (1, 5)))
-
-        self.assertFalse(intersect((1, 6), (2, 5)))
-        self.assertFalse(intersect((2, 5), (1, 6)))
-
-        self.assertTrue(intersect((1, 5), (2, 6)))
-        self.assertTrue(intersect((2, 6), (1, 5)))
+        self.assertFalse(intersect(1, 2, 2, 3))
+        self.assertFalse(intersect(2, 3, 1, 2))
+        self.assertFalse(intersect(1, 5, 2, 5))
+        self.assertFalse(intersect(2, 5, 1, 5))
+        self.assertFalse(intersect(1, 6, 2, 5))
+        self.assertFalse(intersect(2, 5, 1, 6))
+        self.assertTrue(intersect(1, 5, 2, 6))
+        self.assertTrue(intersect(2, 6, 1, 5))
 
 
 def main():
@@ -66,10 +56,10 @@ def main():
 
     if is_sample:
         failures += check(1, part1(parse("sample1.txt"), 8), 4)
-        failures += check(2, part2(parse("sample2.txt"), 8), 21)
+        failures += check(2, part2(parse("sample2.txt")), 21)
     else:
         failures += check(1, part1(parse("input1.txt"), 32), 59)
-        failures += check(2, part2(parse("input2.txt"), 256), 2925233)
+        failures += check(2, part2(parse("input2.txt")), 2925233)
 
     exit(failures)
 
