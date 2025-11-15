cce348f00e577e6166885ee8f688cfda69cbd45e
Frank Blendinger <yogan@zogan.de>
2025-11-15 10:51:57 +0100

    Fix EC2025 Quest 2 build script

--- everybody-codes/2025/quest-02-python/build-ci.sh
+++ everybody-codes/2025/quest-02-python/build-ci.sh
@@ -2,9 +2,8 @@
 set -euo pipefail
 cd "$(dirname "$0")" || exit 1
 
-
 # Save some time in CI: build/venv is only needed for visualization (PIL+tqdm)
-[ -n "${GITHUB_RUN_ID}" ] && return
+[ -n "${GITHUB_RUN_ID}" ] && exit 0
 
 if [ -f requirements.txt ]; then
     pypy3 -m venv venv
