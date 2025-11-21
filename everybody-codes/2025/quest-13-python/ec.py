1beaa491cb33f020bde5218f61336370eef5e197
Frank Blendinger <yogan@zogan.de>
2025-11-21 17:07:39 +0100

    Add EC2025 Quest 13 Part 2 (Python)

--- everybody-codes/2025/quest-13-python/ec.py
+++ everybody-codes/2025/quest-13-python/ec.py
@@ -3,26 +3,48 @@ import unittest
 from collections import deque
 
 
-def parse(filename):
+def parse_part1(filename):
     return list(map(int, open(filename).readlines()))
 
 
+def parse_part2(filename):
+    return [
+        tuple(map(int, line.strip().split("-")))  #
+        for line in open(filename).readlines()
+    ]
+
+
 def part1(xs):
-    wheel = fill(xs)
+    wheel = fill(1, xs)
     return wheel[2025 % len(wheel)]
 
 
-def fill(xs):
-    q = deque([1])
-    back = True
+def part2(xs):
+    wheel = fill((1, 1), xs)
+    required = 20252025 % sum(abs(r - l) + 1 for l, r in wheel)
+    rotations = 0
+
+    for left, right in wheel:
+        dist = required - rotations
+        size = abs(right - left) + 1
+
+        if dist < size:
+            return left + dist if left < right else left - dist
+
+        rotations += size
+
+
+def fill(initial, xs):
+    q = deque([initial])
+    clockwise = True
     offset = 0
     for x in xs:
-        if back:
+        if clockwise:
             q.append(x)
         else:
-            q.appendleft(x)
+            q.appendleft((x[1], x[0]) if isinstance(x, tuple) else x)
             offset += 1
-        back = not back
+        clockwise = not clockwise
     q.rotate(-offset)
     return q
 
@@ -32,16 +54,25 @@ class Tests(unittest.TestCase):
         self.assertEqual(part1([72, 58, 47, 61, 67]), 67)
 
     def test_fill(self):
-        self.assertEqual(fill([72, 58, 47, 61, 67]), deque([1, 72, 47, 67, 61, 58]))
+        self.assertEqual(
+            fill(1, [72, 58, 47, 61, 67]),
+            deque([1, 72, 47, 67, 61, 58]),
+        )
+        self.assertEqual(
+            fill((1, 1), [(10, 15), (12, 13), (20, 21), (19, 23), (30, 37)]),
+            deque([(1, 1), (10, 15), (20, 21), (30, 37), (23, 19), (13, 12)]),
+        )
 
 
 def main():
     failures = 0
 
     if is_sample:
-        failures += check(1, part1(parse("sample1.txt")), 67)
+        failures += check(1, part1(parse_part1("sample1.txt")), 67)
+        failures += check(2, part2(parse_part2("sample2.txt")), 30)
     else:
-        failures += check(1, part1(parse("input1.txt")), 511)
+        failures += check(1, part1(parse_part1("input1.txt")), 511)
+        failures += check(2, part2(parse_part2("input2.txt")), 4702)
 
     exit(failures)
 
