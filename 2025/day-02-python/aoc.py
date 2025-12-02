7a7291886f6c046d645220022710873d9765ac2e
Frank Blendinger <yogan@zogan.de>
2025-12-02 10:07:49 +0100

    Add AoC 2025 day 02 part 2 (Python)

--- 2025/day-02-python/aoc.py
+++ 2025/day-02-python/aoc.py
@@ -11,36 +11,68 @@ def parse():
 
 
 def part1(ranges):
+    return sum_invalid_ids(ranges, part=1)
+
+
+def part2(ranges):
+    return sum_invalid_ids(ranges, part=2)
+
+
+def sum_invalid_ids(ranges, part):
     total = 0
 
     for lo, hi in ranges:
         for id in range(lo, hi + 1):
-            if invalid(id):
+            if part == 1 and repeated_once(str(id)):
+                total += id
+            elif part == 2 and repeated_seqs(str(id)):
                 total += id
 
     return total
 
 
-def invalid(id):
-    s = str(id)
-    m = len(s) // 2
-    return s[m:] == s[:m]
+def repeated_once(id):
+    m = len(id) // 2
+    return id[m:] == id[:m]
+
+
+def repeated_seqs(id):
+    l = len(id)
+
+    for i in range(1, l // 2 + 1):
+        seq = id[:i]
+        if id == seq * (l // len(seq)):
+            return True
+
+    return False
 
 
 class Tests(unittest.TestCase):
-    def test_invalid(self):
-        self.assertTrue(invalid(11))
-        self.assertTrue(invalid(1010))
-        self.assertTrue(invalid(1188511885))
+    def test_repeated_once(self):
+        self.assertTrue(repeated_once("11"))
+        self.assertTrue(repeated_once("1010"))
+        self.assertTrue(repeated_once("1188511885"))
 
-        self.assertFalse(invalid(10))
-        self.assertFalse(invalid(101))
-        self.assertFalse(invalid(1188511858))
+        self.assertFalse(repeated_once("10"))
+        self.assertFalse(repeated_once("101"))
+        self.assertFalse(repeated_once("1188511858"))
+
+    def test_repeated_seqs(self):
+        self.assertTrue(repeated_seqs("11"))
+        self.assertTrue(repeated_seqs("999"))
+        self.assertTrue(repeated_seqs("1010"))
+        self.assertTrue(repeated_seqs("1188511885"))
+
+        self.assertFalse(repeated_seqs("12"))
+        self.assertFalse(repeated_seqs("998"))
+        self.assertFalse(repeated_seqs("10101"))
+        self.assertFalse(repeated_seqs("118851188"))
 
 
 def main():
     failures = 0
     failures += check(1, part1(parse()), 1227775554 if is_sample else 41294979841)
+    failures += check(2, part2(parse()), 4174379265 if is_sample else 66500947346)
 
     exit(failures)
 
