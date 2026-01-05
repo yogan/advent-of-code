d9f64c88cbabfcc88265b79751a2616f61ed689a
Frank Blendinger <yogan@zogan.de>
2026-01-05 11:52:30 +0100

    Refactor 2025 day 9 in F#

--- 2025/day-09-fsharp/AoC.fs
+++ 2025/day-09-fsharp/AoC.fs
@@ -7,44 +7,46 @@ type Edge = (Coord * Coord)
 type Rectangle = (int * int * int * int)
 
 module AoC =
-    let Edges (coords: Coord seq) : Edge list =
+    let area (c1, r1) (c2, r2) =
+        int64 (abs (c2 - c1) + 1) * int64 (abs (r2 - r1) + 1)
+
+    let combinations list =
+        list
+        |> List.indexed
+        |> List.collect (fun (i, x) -> list |> List.skip i |> List.map (fun y -> x, y))
+
+    let edges (coords: Coord seq) : Edge list =
         let cs = coords |> Seq.toList
         let csShifted = List.tail cs @ [ List.head cs ]
         List.zip cs csShifted |> List.map (fun (l, r) -> if l <= r then l, r else r, l)
 
-    let Inside (p1: Coord) (p2: Coord) (rectangle: Rectangle) : bool =
-        let c1, r1 = p1
-        let c2, r2 = p2
+    let edgeWithin (edges: Edge list) (c1: int, r1: int) (c2: int, r2: int) : bool =
+        let rectangle = min c1 c2, max c1 c2, min r1 r2, max r1 r2
         let cMin, cMax, rMin, rMax = rectangle
 
-        if r1 = r2 then
-            rMin < r1 && r1 < rMax && c1 < cMax && cMin < c2
-        else
-            cMin < c1 && c1 < cMax && r1 < rMax && rMin < r2
+        let inside (p1: Coord) (p2: Coord) =
+            let c1, r1 = p1
+            let c2, r2 = p2
 
-    let Solve (coords: Coord list) : (int64 * int64) =
-        let mutable p1 = 0L
-        let mutable p2 = 0L
-        let E = Edges coords
+            if r1 = r2 then
+                rMin < r1 && r1 < rMax && c1 < cMax && cMin < c2
+            else
+                cMin < c1 && c1 < cMax && r1 < rMax && rMin < r2
 
-        coords
-        |> List.iteri (fun i (c1, r1) ->
-            coords
-            |> List.skip i
-            |> List.iter (fun (c2, r2) ->
-                let area = int64 (abs (c2 - c1) + 1) * int64 (abs (r2 - r1) + 1)
-                p1 <- max p1 area
+        edges |> List.exists (fun (e1, e2) -> inside e1 e2)
 
-                if area >= p2 then
-                    let rectangle = min c1 c2, max c1 c2, min r1 r2, max r1 r2
-                    let hasEdgeInside = E |> List.exists (fun (e1, e2) -> Inside e1 e2 rectangle)
+    let solve (coords: Coord list) : (int64 * int64) =
+        let edges = edges coords
 
-                    if not hasEdgeInside then
-                        p2 <- area))
+        let tryPair (p1, p2) (c1, c2) =
+            let a = area c1 c2
+            let p1 = max p1 a
+            let p2 = if a >= p2 && not (edgeWithin edges c1 c2) then a else p2
+            p1, p2
 
-        p1, p2
+        coords |> combinations |> List.fold tryPair (0L, 0L)
 
-    let ParseInput (lines: string seq) : Coord list =
+    let parse (lines: string seq) : Coord list =
         lines
         |> Seq.map (fun line ->
             let parts = line.Split ',' |> Array.map int
@@ -54,13 +56,13 @@ module AoC =
 
 type AoCTests() =
     [<Fact>]
-    member _.``ParseInput works for sample``() =
+    member _.``parse works for sample``() =
         let sample = [| "7,1"; "11,1"; "11,7"; "9,7"; "9,5"; "2,5"; "2,3"; "7,3" |]
         let expected = [ 7, 1; 11, 1; 11, 7; 9, 7; 9, 5; 2, 5; 2, 3; 7, 3 ]
-        Assert.Equal<Coord list>(expected, AoC.ParseInput sample)
+        Assert.Equal<Coord list>(expected, AoC.parse sample)
 
     [<Fact>]
-    member _.``Edges works for sample``() =
+    member _.``edges works for sample``() =
         let coords = [ 7, 1; 11, 1; 11, 7; 9, 7; 9, 5; 2, 5; 2, 3; 7, 3 ]
 
         let expected: Edge list =
@@ -73,19 +75,19 @@ type AoCTests() =
               (2, 3), (7, 3)
               (7, 1), (7, 3) ]
 
-        Assert.Equal<Edge list>(expected, AoC.Edges coords)
+        Assert.Equal<Edge list>(expected, AoC.edges coords)
 
     [<Fact>]
-    member _.``Solve works for the sample``() =
+    member _.``solve works for the sample``() =
         let coords = [ 7, 1; 11, 1; 11, 7; 9, 7; 9, 5; 2, 5; 2, 3; 7, 3 ]
-        Assert.Equal((50L, 24L), AoC.Solve coords)
+        Assert.Equal((50L, 24L), AoC.solve coords)
 
 
 let p1, p2 =
     Environment.GetCommandLineArgs()[1]
     |> File.ReadAllLines
-    |> AoC.ParseInput
-    |> AoC.Solve
+    |> AoC.parse
+    |> AoC.solve
 
 printfn "%d" p1
 printfn "%d" p2
